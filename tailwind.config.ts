b0914676f2d89cb7b53fddcea6c30fb66bda7c22
Timeuh <timeuuh@gmail.com>
2023-08-19 21:26:16 +0200

    style: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,24 @@ import type {Config} from 'tailwindcss';
 const config: Config = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}'
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        light: '#FAFAFA',
+        'light-blue': '#A4DDED',
+        'light-purple': '#FAE6FA',
+        'light-silver': '#989898',
+        dark: '#1A1A1A',
+        'dark-blue': '#191970',
+        'dark-purple': '#301934',
+        'dark-silver': '#ECECEC',
+        bronze: '#E5A770',
+        gold: '#FFD231',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
 export default config;
