77a58af1d8ff884addb93b37c1796b5c7524dee3
Timeuh <timeuuh@gmail.com>
2023-08-20 09:00:02 +0200

    feat: create navbar component for mobile

---
+++ app/(components)/Navbar.tsx
@@ -0,0 +1,15 @@
+import LightContainer from '@components/LightContainer';
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <div className={'fixed flex h-16 w-full flex-row items-center justify-between bg-light/30 px-6 backdrop-blur-sm'}>
+      <LightContainer height={'h-12'} width={'w-12'}>
+        <Image src={'/img/navbar/moon.png'} alt={'moon'} className={'h-8 w-8'} width={32} height={32} />
+      </LightContainer>
+      <LightContainer height={'h-12'} width={'w-12'}>
+        <Image src={'/img/navbar/menu-light.png'} alt={'menu'} className={'h-8 w-8'} width={32} height={32} />
+      </LightContainer>
+    </div>
+  );
+}
