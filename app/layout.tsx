f60f9c3dec6c9578c4deda2d3b1412676e94b77c
Timeuh <timeuuh@gmail.com>
2023-08-20 09:02:10 +0200

    feat: use navbar in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,22 @@ import './globals.css';
 import type {Metadata} from 'next';
 import {Nunito} from 'next/font/google';
 import {ReactNode} from 'react';
+import Navbar from '@components/Navbar';
 
 const nunito = Nunito({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'WankulDex',
-  description: 'Un site pour consulter vos cartes Wankul'
+  description: 'Un site pour consulter vos cartes Wankul',
 };
 
 export default function RootLayout({children}: {children: ReactNode}) {
   return (
     <html lang='fr'>
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
