af984309e10a66cb40ee366a4fb7a1bae3343d49
Timeuh <timeuuh@gmail.com>
2023-08-23 18:23:12 +0200

    style: fit card with infos

--- app/_components/FieldCard.tsx
+++ app/_components/FieldCard.tsx
@@ -8,22 +8,23 @@ type Props = {
 
 export default function FieldCard({card}: Props) {
   return (
-    <div id={`field_${card.card.id}`} className={'relative mt-6 flex h-[305px] flex-col items-center justify-end'}>
-      <Image
-        src={`${API_DOMAIN}${card.links.image}`}
-        alt={card.card.name}
-        width={250}
-        height={250}
-        className={'absolute -top-[44px] h-[82vw] rotate-90 rounded-l-lg border-2 border-dark'}
-      />
+    <div
+      id={`field_${card.card.id}`}
+      className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center xl:h-[40vh] xl:w-[25vw]'}
+    >
+      <div className={'relative h-[50vh] w-[82vw] rounded-t-lg border-2 border-b-0 border-dark xl:w-[25vw]'}>
+        <Image src={`${API_DOMAIN}${card.links.image}`} alt={card.card.name} className={'rounded-t-lg'} fill={true} />
+      </div>
       <div
         className={
-          'flex w-[82vw] flex-row items-center justify-between rounded-b-lg border-2 border-dark bg-light px-2'
+          'flex w-[82vw] flex-row items-center justify-between rounded-b-lg border-2 border-dark bg-light px-2 xl:w-[25vw]'
         }
       >
         <div className={'flex h-12 flex-row items-center justify-start space-x-2'}>
-          <Image src={'/img/cards-light.png'} alt={'cards-logo'} width={24} height={24} />
-          <h1 className={'font-bold'}>{card.card.name.toUpperCase()}</h1>
+          <div className={'relative h-10 w-10'}>
+            <Image src={'/img/cards-light.png'} alt={'cards-logo'} fill={true} />
+          </div>
+          <h1 className={'font-bold xl:text-xl'}>{card.card.name.toUpperCase()}</h1>
         </div>
         <h2 className={'h-10 w-10 rounded-full border-2 border-dark pt-2 text-center font-bold'}>{card.card.id}</h2>
       </div>
