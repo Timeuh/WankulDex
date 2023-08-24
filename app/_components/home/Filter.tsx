5c80d3d243fc2f6f2a5813ae249bd9b05a4e4a29
Timeuh <timeuuh@gmail.com>
2023-08-24 20:55:15 +0200

    feat: use hook to get all types

--- app/_components/home/Filter.tsx
+++ app/_components/home/Filter.tsx
@@ -4,11 +4,13 @@ import LightContainer from '@components/LightContainer';
 import Image from 'next/image';
 import {useState} from 'react';
 import useArtists from '@/app/_hooks/useArtists';
-import {Artist, Artists} from '@/app/_utils/appTypes';
+import {Artist, Type} from '@/app/_utils/appTypes';
+import useTypes from '@/app/_hooks/useTypes';
 
 export default function Filter() {
   const [isActive, setActive] = useState<boolean>(false);
-  const {artists, setArtists} = useArtists();
+  const {artists} = useArtists();
+  const {types} = useTypes();
 
   const toggleActive = () => {
     setActive(!isActive);
@@ -21,29 +23,43 @@ export default function Filter() {
       </LightContainer>
       <div
         className={`absolute top-16 z-10 w-[83vw] transform duration-300 xl:w-[49.5vw] ${
-          isActive ? 'h-[50vh] opacity-100' : 'h-0 opacity-0'
+          isActive ? 'h-fit opacity-100' : 'h-0 opacity-0'
         }`}
       >
         <LightContainer height={'h-full'} width={'w-full'} hover={false}>
           <div className={'grid h-full w-full grid-cols-2'}>
-            <div className={'m-2 flex h-3/4 w-5/6 flex-col border-b-2 border-[#CCCCCCFF] p-2'}>
-              <div className={'flex flex-row items-center space-x-4'}>
+            <div className={'m-2 flex h-fit w-5/6 flex-col overflow-hidden border-b-2 border-[#CCCCCCFF] p-2'}>
+              <div className={'flex flex-row items-center space-x-4 pb-2'}>
                 <div className={'relative h-8 w-8 xl:h-10 xl:w-10'}>
                   <Image src={'/img/home/types-filter-light.png'} alt={'type icon'} fill={true} />
                 </div>
                 <h1 className={'text-xl text-wankil-purple xl:text-3xl'}>Types</h1>
               </div>
+              {types.map((type: Type) => {
+                return (
+                  <div key={type.type.id} className={'flex flex-row-reverse items-center justify-between py-1'}>
+                    <label htmlFor={type.type.name} className={'xl:text-xl'}>
+                      {type.type.name}
+                    </label>
+                    <input
+                      type='checkbox'
+                      value={type.type.name}
+                      className={'h-4 w-4 rounded-md accent-dark xl:h-6 xl:w-6'}
+                    />
+                  </div>
+                );
+              })}
             </div>
-            <div className={'m-2 flex h-3/4 w-5/6 flex-col border-b-2 border-[#CCCCCCFF] py-2'}>
-              <div className={'flex w-full flex-row items-center'}>
+            <div className={'m-2 flex h-fit w-5/6 flex-col border-b-2 border-[#CCCCCCFF] py-2'}>
+              <div className={'flex w-full flex-row items-center pb-2'}>
                 <div className={'relative h-8 w-8 xl:h-10 xl:w-10'}>
                   <Image src={'/img/home/wankul-filter-light.png'} alt={'type icon'} fill={true} />
                 </div>
                 <h1 className={'text-xl text-wankil-blue xl:text-3xl'}>Personnages</h1>
               </div>
             </div>
-            <div className={'m-2 flex h-3/4 w-5/6 flex-col overflow-hidden border-b-2 border-[#CCCCCCFF] p-2'}>
-              <div className={'flex flex-row items-center space-x-4'}>
+            <div className={'m-2 flex h-fit w-5/6 flex-col overflow-hidden border-b-2 border-[#CCCCCCFF] p-2'}>
+              <div className={'flex flex-row items-center space-x-4 pb-2'}>
                 <div className={'relative h-8 w-8 xl:h-10 xl:w-10'}>
                   <Image src={'/img/home/pen-filter-light.png'} alt={'type icon'} fill={true} />
                 </div>
@@ -64,8 +80,8 @@ export default function Filter() {
                 );
               })}
             </div>
-            <div className={'m-2 flex h-3/4 w-5/6 flex-col border-b-2 border-[#CCCCCCFF] p-2'}>
-              <div className={'flex flex-row items-center space-x-4'}>
+            <div className={'m-2 flex h-fit w-5/6 flex-col border-b-2 border-[#CCCCCCFF] p-2'}>
+              <div className={'flex flex-row items-center space-x-4 pb-2'}>
                 <div className={'relative h-8 w-8 xl:h-10 xl:w-10'}>
                   <Image src={'/img/home/rarity-light.png'} alt={'type icon'} fill={true} />
                 </div>
