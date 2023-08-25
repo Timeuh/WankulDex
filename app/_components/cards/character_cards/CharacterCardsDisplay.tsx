8308278ef2e99b1aa352a96f88b8861a54fd6f98
Timeuh <timeuuh@gmail.com>
2023-08-25 21:37:08 +0200

    feat: add isActive state + use CardPreview component

--- app/_components/cards/character_cards/CharacterCardsDisplay.tsx
+++ app/_components/cards/character_cards/CharacterCardsDisplay.tsx
@@ -4,12 +4,20 @@ import {Card} from '@/app/_utils/appTypes';
 import {CARD_TYPE_FIELD} from '@/app/_utils/appConsts';
 import CharacterCard from '@components/cards/character_cards/CharacterCard';
 import CharacterCardsLoading from '@components/cards/character_cards/CharacterCardsLoading';
+import {useState} from 'react';
+import CardPreview from '@components/cards/CardPreview';
 
 type Props = {
   cards: Array<Card>;
 };
 
 export default function CharacterCardsDisplay({cards}: Props) {
+  const [isActive, setActive] = useState<boolean>(false);
+
+  const handleActivation = () => {
+    setActive(!isActive);
+  };
+
   return (
     <section id={'character cards'} className={'flex w-[83%] flex-col items-center xl:w-full'}>
       <LightContainer height={'h-12'} width={'w-full xl:w-1/3'} hover={false}>
@@ -24,7 +32,15 @@ export default function CharacterCardsDisplay({cards}: Props) {
         <div className={'flex grid-cols-4 flex-col items-center xl:grid xl:gap-8'}>
           {cards.map((card: Card) => {
             if (card.card.type.name !== CARD_TYPE_FIELD) {
-              return <CharacterCard key={card.card.id} card={card} />;
+              return (
+                <CardPreview
+                  key={card.card.id}
+                  componentBase={<CharacterCard card={card} handleActivation={handleActivation} />}
+                  componentPreview={<CharacterCard card={card} />}
+                  isActive={isActive}
+                  handleActivation={handleActivation}
+                />
+              );
             }
           })}
         </div>
