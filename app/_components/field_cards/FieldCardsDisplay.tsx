31f1e3052f131a8efa02eb681e88b2b64faba1dd
Timeuh <timeuuh@gmail.com>
2023-08-25 21:31:43 +0200

    feat: use card preview for field cards

--- app/_components/field_cards/FieldCardsDisplay.tsx
+++ app/_components/field_cards/FieldCardsDisplay.tsx
@@ -4,12 +4,20 @@ import {Card} from '@/app/_utils/appTypes';
 import {CARD_TYPE_FIELD} from '@/app/_utils/appConsts';
 import FieldCard from '@components/field_cards/FieldCard';
 import FieldCardsLoading from '@components/field_cards/FieldCardsLoading';
+import CardPreview from '@components/field_cards/CardPreview';
+import {useState} from 'react';
 
 type Props = {
   cards: Array<Card>;
 };
 
 export default function FieldCardsDisplay({cards}: Props) {
+  const [isActive, setActive] = useState<boolean>(false);
+
+  const handleActivation = () => {
+    setActive(!isActive);
+  };
+
   return (
     <section id={'field cards'} className={'flex w-[83%] flex-col items-center xl:w-full'}>
       <LightContainer height={'h-12'} width={'w-full xl:w-1/3'} hover={false}>
@@ -24,7 +32,15 @@ export default function FieldCardsDisplay({cards}: Props) {
         <div className={'flex grid-cols-3 flex-col items-center xl:grid xl:gap-6'}>
           {cards.map((card: Card) => {
             if (card.card.type.name === CARD_TYPE_FIELD) {
-              return <FieldCard key={card.card.id} card={card} />;
+              return (
+                <CardPreview
+                  key={card.card.id}
+                  componentBase={<FieldCard card={card} handleActivation={handleActivation} />}
+                  componentPreview={<FieldCard card={card} />}
+                  isActive={isActive}
+                  handleActivation={handleActivation}
+                />
+              );
             }
           })}
         </div>
