2fdb265e2930bc3ff1395634cf79194d6a03a795
Timeuh <timeuuh@gmail.com>
2023-08-25 18:28:08 +0200

    feat: create cards loading components

---
+++ app/_components/character_cards/CharacterCardsLoading.tsx
@@ -0,0 +1,10 @@
+export default function CharacterCardsLoading() {
+  return (
+    <div className={'flex grid-cols-4 flex-col items-center xl:grid xl:gap-8'}>
+      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
+      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
+      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
+      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
+    </div>
+  );
+}
---
+++ app/_components/field_cards/FieldCardsLoading.tsx
@@ -0,0 +1,9 @@
+export default function FieldCardsLoading() {
+  return (
+    <div className={'flex grid-cols-3 flex-col items-center xl:grid xl:gap-6'}>
+      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
+      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
+      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
+    </div>
+  );
+}
