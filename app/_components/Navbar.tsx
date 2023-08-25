eb9787fb1209630a8cc88138284ab91ecce60ed3
Timeuh <timeuuh@gmail.com>
2023-08-25 19:07:33 +0200

    style: put navbar on top of everything

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <div
-      className={'fixed z-10 flex h-16 w-full flex-row items-center justify-between bg-light/30 px-6 backdrop-blur-sm'}
+      className={'fixed z-30 flex h-16 w-full flex-row items-center justify-between bg-light/30 px-6 backdrop-blur-sm'}
     >
       <LightContainer height={'h-12'} width={'w-12'} hover={true}>
         <Image src={'/img/navbar/moon.png'} alt={'moon'} className={'h-8 w-8'} width={32} height={32} />
