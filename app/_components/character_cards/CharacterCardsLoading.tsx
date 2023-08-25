59ad3ca69369f0dfc49b81031aa41b556125e835
Timeuh <timeuuh@gmail.com>
2023-08-25 18:35:14 +0200

    feat: use fake cards array to display loading cards

--- app/_components/character_cards/CharacterCardsLoading.tsx
+++ app/_components/character_cards/CharacterCardsLoading.tsx
@@ -1,10 +1,21 @@
 export default function CharacterCardsLoading() {
+  const fakeCards = [];
+  const maxCards = 4;
+
+  for (let i = 0; i < maxCards; i++) {
+    fakeCards.push(i);
+  }
+
   return (
     <div className={'flex grid-cols-4 flex-col items-center xl:grid xl:gap-8'}>
-      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
-      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
-      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
-      <div className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}></div>
+      {fakeCards.map((index) => {
+        return (
+          <div
+            key={index}
+            className={'my-6 flex h-[65vh] w-[82vw] flex-col items-center bg-dark xl:h-[60vh] xl:w-[20vw]'}
+          ></div>
+        );
+      })}
     </div>
   );
 }
--- app/_components/field_cards/FieldCardsLoading.tsx
+++ app/_components/field_cards/FieldCardsLoading.tsx
@@ -1,9 +1,21 @@
 export default function FieldCardsLoading() {
+  const fakeCards = [];
+  const maxCards = 3;
+
+  for (let i = 0; i < maxCards; i++) {
+    fakeCards.push(i);
+  }
+
   return (
     <div className={'flex grid-cols-3 flex-col items-center xl:grid xl:gap-6'}>
-      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
-      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
-      <div className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}></div>
+      {fakeCards.map((index) => {
+        return (
+          <div
+            key={index}
+            className={'my-6 flex h-[32vh] w-[82vw] flex-col items-center bg-dark xl:h-[40vh] xl:w-[25vw]'}
+          ></div>
+        );
+      })}
     </div>
   );
 }
