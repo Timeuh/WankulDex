81e8fdde5bb7c707ba7eb5078a0ef2b8c3dbe971
Timeuh <timeuuh@gmail.com>
2023-08-30 21:55:24 +0200

    feat: create array of navbar links

---
+++ app/_utils/navbarLinks.ts
@@ -0,0 +1,34 @@
+import {NavbarLink} from '@/app/_utils/appTypes';
+
+export const links: Array<NavbarLink> = [
+  {
+    image: '/img/navbar/home-light.png',
+    name: 'Accueil',
+    link: '/',
+  },
+  {
+    image: '/img/navbar/pen-light.png',
+    name: 'Artistes',
+    link: '/artist',
+  },
+  {
+    image: '/img/navbar/wankul-light.png',
+    name: 'Personnages',
+    link: '/character',
+  },
+  {
+    image: '/img/navbar/rarity-navbar-light.png',
+    name: 'Raretés',
+    link: '/rarity',
+  },
+  {
+    image: '/img/cards-light.png',
+    name: 'Types',
+    link: '/type',
+  },
+  {
+    image: '/img/admin/login/admin-light.png',
+    name: 'Admin',
+    link: '/admin/login',
+  },
+];
