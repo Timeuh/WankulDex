434c5078ddaf6248962eb8485eaaa7c6d25bb151
Timeuh <timeuuh@gmail.com>
2023-08-30 19:27:43 +0200

    feat: create card creation page placeholder

---
+++ app/admin/create/card/page.tsx
@@ -0,0 +1,13 @@
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Wankuldex - Créer un carte',
+};
+
+export default function CreateCard() {
+  return (
+    <main>
+      <h1>Page création de cartes</h1>
+    </main>
+  );
+}
