613f6bcf5406c4e208634c3ee405d894258c7c26
Timeuh <timeuuh@gmail.com>
2023-08-31 18:40:33 +0200

    feat: create navbar menu to display all navbar links

---
+++ app/_components/navbar/NavbarMenu.tsx
@@ -0,0 +1,28 @@
+import {links} from '@/app/_utils/navbarLinks';
+import {NavbarLink} from '@/app/_utils/appTypes';
+import NavbarLinkDisplay from '@components/navbar/NavbarLinkDisplay';
+import LightContainer from '@components/LightContainer';
+
+type Props = {
+  isActive: boolean;
+  onClick: () => void;
+};
+
+export default function NavbarMenu({isActive, onClick}: Props) {
+  return (
+    <div
+      className={`absolute right-0 flex h-auto w-auto flex-row items-center transition-transform duration-500 ${
+        isActive ? 'translate-x-0' : 'translate-x-96'
+      }`}
+      onClick={onClick}
+    >
+      <LightContainer width={'w-full'} height={'h-full'} hover={false}>
+        <div className={'flex w-full flex-col items-center'}>
+          {links.map((link: NavbarLink, index: number) => {
+            return <NavbarLinkDisplay key={link.name} link={link} isLast={index === links.length - 1} />;
+          })}
+        </div>
+      </LightContainer>
+    </div>
+  );
+}
