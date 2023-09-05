df01a7d204c42b663e393a9b155d92756db6f0c4
Timeuh <timeuuh@gmail.com>
2023-09-05 21:30:54 +0200

    feat: use card creation form in dedicated page

--- app/admin/create/card/page.tsx
+++ app/admin/create/card/page.tsx
@@ -1,4 +1,8 @@
 import {Metadata} from 'next';
+import Image from 'next/image';
+import BackButton from '@components/BackButton';
+import FormInput from '@components/form/FormInput';
+import CardForm from '@components/admin/create/cards/CardForm';
 
 export const metadata: Metadata = {
   title: 'Wankuldex - Créer un carte',
@@ -6,8 +10,24 @@ export const metadata: Metadata = {
 
 export default function CreateCard() {
   return (
-    <main>
-      <h1>Page création de cartes</h1>
+    <main
+      className={
+        'flex h-full w-full flex-col items-center space-y-6 bg-gradient-to-b from-light-purple to-light-blue pb-6'
+      }
+    >
+      <BackButton />
+      <div className={'flex flex-col items-center space-y-2'}>
+        <Image
+          src={'/img/cards-light.png'}
+          alt={'carte'}
+          width={0}
+          height={0}
+          sizes={'100vw'}
+          className={'h-auto w-14'}
+        />
+        <h1 className={'text-3xl text-dark'}>Nouvelle Carte</h1>
+      </div>
+      <CardForm />
     </main>
   );
 }
