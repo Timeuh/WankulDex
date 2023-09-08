a0a1cbd9c7ba02c8bbd204d4029ee3cdfd11dcbc
Timeuh <timeuuh@gmail.com>
2023-09-08 14:12:05 +0200

    feat: create method to create a card description

---
+++ app/_utils/createCardDescription.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import {cookies} from 'next/headers';
+import {API_DOMAIN} from '@/app/_utils/appConsts';
+
+export default async function createCardDescription(cardParameters: Object) {
+  const authCookie = cookies().get('wankul-admin-token');
+  const authHeader = process.env.API_AUTH_HEADER;
+
+  if (!authCookie || !authHeader) {
+    return false;
+  }
+
+  return await fetch(`${API_DOMAIN}/descriptions/new`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      [authHeader]: authCookie.value,
+    },
+    body: JSON.stringify(cardParameters),
+  }).then((response) => {
+    return response.json();
+  });
+}
