40c490615ab7a5b6f05d1646dbd45fea4d90fb78
Timeuh <timeuuh@gmail.com>
2023-09-10 22:20:33 +0200

    feat: create landing section content (WIP)

--- app/_components/home/Landing.tsx
+++ app/_components/home/Landing.tsx
@@ -1,7 +1,56 @@
+import Image from 'next/image';
+
 export default function Landing() {
   return (
-    <section id={'Landing'} className={'h-screen w-full bg-gradient-to-b from-light-blue to-light-purple'}>
-      <h1>Landing</h1>
+    <section
+      id={'Landing'}
+      className={
+        'flex h-screen w-full flex-col items-center space-y-6 bg-gradient-to-b from-light-blue to-light-purple'
+      }
+    >
+      <Image
+        src={'/img/home/wankul-logo-light.png'}
+        alt={'logo wankul'}
+        width={0}
+        height={0}
+        sizes={'100vw'}
+        className={'h-auto w-4/5 pt-24'}
+        priority
+      />
+      <div id={'wankuldex-description'} className={'w-4/5 rounded-lg border-2 border-dark-purple py-6 text-center'}>
+        <p className={'text-xl text-dark-blue'}>
+          Le <span className={'font-bold text-dark-purple'}>WankulDex</span>, c&apos;est le site qui te permet
+          d&apos;admirer les magnifiques cartes <span className={'font-bold text-dark-purple'}>Wankul </span>
+          avec de jolis effets !
+        </p>
+      </div>
+      <div
+        id={'wankul-link'}
+        className={
+          'flex h-fit w-4/5 flex-col items-center rounded-lg border-2 border-dark bg-[#FFD231] text-center text-xl text-dark'
+        }
+      >
+        <div
+          className={
+            'flex h-16 w-full flex-col items-center justify-center rounded-b-lg bg-dark text-2xl font-bold text-[#FFD231]'
+          }
+        >
+          <h1>⚠️ ATTENTION !</h1>
+        </div>
+        <p className={'w-4/5 py-2'}>Ce site n&apos;est en aucun cas affilié aux cartes Wankul !</p>
+        <h2 className={'w-4/5'}>
+          Pour voir le <span className={'font-bold'}>site officiel</span> :
+        </h2>
+        <button className={'my-2 h-14 w-1/2 rounded-lg bg-light'}>Wankul.fr</button>
+      </div>
+      <Image
+        src={'/img/home/down-light.png'}
+        alt={'voir la suite'}
+        width={0}
+        height={0}
+        sizes={'100vw'}
+        className={'h-auto w-1/4 pt-6'}
+      />
     </section>
   );
 }
