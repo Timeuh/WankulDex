0a2973cb8a920dd17e0160bf4005a6d79860dbf0
Timeuh <timeuuh@gmail.com>
2023-09-10 21:41:21 +0200

    feat: create landing page component

---
+++ app/_components/home/Landing.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <section id={'Landing'} className={'h-screen w-full bg-gradient-to-b from-light-blue to-light-purple'}>
+      <h1>Landing</h1>
+    </section>
+  );
+}
