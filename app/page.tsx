812b5173c68a110de7985e98dfa01eb74083dcb5
Timeuh <timeuuh@gmail.com>
2023-09-12 10:46:18 +0200

    feat: use home search

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Landing from '@components/home/Landing';
+import HomeSearch from '@components/home/HomeSearch';
 
 export default function Home() {
   return (
     <section id={'home'}>
       <Landing />
+      <section id={'cards-wrapper'} className={'h-full w-full bg-gradient-to-b from-light-purple to-light-blue'}>
+        <HomeSearch />
+      </section>
     </section>
   );
 }
