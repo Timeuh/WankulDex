b9c168c6cafc64cbca21a4c934c6134219cbbd50
Timeuh <timeuuh@gmail.com>
2023-09-12 11:41:49 +0200

    feat: create field cards display component (WIP)

---
+++ app/_components/home/FieldCards.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import BaseContainer from '@components/BaseContainer';
+
+export default function FieldCards() {
+  return (
+    <section id={'field-cards'}>
+      <BaseContainer>
+        <div className={'flex h-12 w-[83vw] flex-row items-center justify-center space-x-4'}>
+          <Image
+            src={'/img/home/type-light.png'}
+            alt={'logo terrains'}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-10'}
+          />
+          <h1 className={'text-3xl text-dark'}>Terrains</h1>
+        </div>
+      </BaseContainer>
+    </section>
+  );
+}
