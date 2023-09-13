1bdc7debc31e70f5a60b959a551e2eca6dcd16c4
Timeuh <timeuuh@gmail.com>
2023-09-13 21:42:01 +0200

    feat: use field card component for each card

--- app/_components/cards/DisplayFieldCards.tsx
+++ app/_components/cards/DisplayFieldCards.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import useCards from '@/app/_hooks/useCards';
+import {Card} from '@/app/_utils/appTypes';
+import FieldCard from '@components/cards/FieldCard';
 
 export default function DisplayFieldCards() {
   const {data, error, isFetching} = useCards('Terrain');
 
-  if (!isFetching) {
-    console.log(data);
-  }
-
   return (
-    <section id={'field-cards-display'}>
-      <h1>terrains</h1>
+    <section id={'field-cards-display'} className={'grid h-full w-full grid-cols-1 space-y-6'}>
+      {data?.map((card: Card) => {
+        return <FieldCard key={card.card.id} card={card} />;
+      })}
     </section>
   );
 }
