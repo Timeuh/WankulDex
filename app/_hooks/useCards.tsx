dbcbf38ed3edb15985bd0e1a097093b59098b172
Timeuh <timeuuh@gmail.com>
2023-09-13 18:59:12 +0200

    feat: create hook to get cards from the api

---
+++ app/_hooks/useCards.tsx
@@ -0,0 +1,26 @@
+import {useQuery} from 'react-query';
+import {API_BASE_URL} from '@/app/_utils/appGlobals';
+import cardSchema from '@/app/_zod/CardSchema';
+import {Card, Cards} from '@/app/_utils/appTypes';
+
+const getCards = async () => {
+  return await fetch(`${API_BASE_URL}/card`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((cards: Cards) => {
+      const allCards: Array<Card> = [];
+
+      cards.cards.forEach((card: Card) => {
+        if (cardSchema.safeParse(card).success) {
+          allCards.push(card);
+        }
+      });
+
+      return allCards;
+    });
+};
+
+export default function useCards() {
+  return useQuery('cards', getCards);
+}
