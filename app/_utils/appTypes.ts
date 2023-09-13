093af18b4ad7d11735fee015225090bf0742f97e
Timeuh <timeuuh@gmail.com>
2023-09-13 18:57:10 +0200

    feat: create app types file

---
+++ app/_utils/appTypes.ts
@@ -0,0 +1,43 @@
+export type Card = {
+  card: {
+    artist: {
+      id: number;
+      name: string;
+    };
+    collection: string;
+    description: {
+      character: {
+        id: number;
+        name: string;
+      };
+      citation: string;
+      effect: string;
+      id: number;
+      looser_effect: string;
+      rarity: {
+        id: number;
+        name: string;
+      };
+      special: string;
+      winner_effect: string;
+    };
+    id: number;
+    image: string;
+    name: string;
+    type: {
+      id: number;
+      name: string;
+    };
+  };
+  links: {
+    all: string;
+    image: string;
+    self: string;
+  };
+};
+
+export type Cards = {
+  cards: Array<Card>;
+  length: number;
+  type: string;
+};
