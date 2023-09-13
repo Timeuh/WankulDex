296c2411d1ee5d63cf5c3a65a6fcc8df0bcee1bf
Timeuh <timeuuh@gmail.com>
2023-09-13 22:37:08 +0200

    feat: use field card loading component when fetching cards

--- app/_components/cards/DisplayFieldCards.tsx
+++ app/_components/cards/DisplayFieldCards.tsx
@@ -3,18 +3,29 @@
 import useCards from '@/app/_hooks/useCards';
 import {Card} from '@/app/_utils/appTypes';
 import FieldCard from '@components/cards/FieldCard';
+import FieldCardLoading from '@components/cards/FieldCardLoading';
 
 export default function DisplayFieldCards() {
   const {data, error, isFetching} = useCards('Terrain');
+  const loadingCards: Array<number> = [];
+  const placeholderCardsNumber = 3;
+
+  for (let i = 0; i < placeholderCardsNumber; i++) {
+    loadingCards.push(i);
+  }
 
   return (
     <section
       id={'field-cards-display'}
       className={'grid h-full w-full grid-cols-1 gap-12 space-y-6 xl:grid-cols-3 xl:space-y-0'}
     >
-      {data?.map((card: Card) => {
-        return <FieldCard key={card.card.id} card={card} />;
-      })}
+      {isFetching
+        ? loadingCards.map((id: number) => {
+            return <FieldCardLoading key={id} />;
+          })
+        : data?.map((card: Card) => {
+            return <FieldCard key={card.card.id} card={card} />;
+          })}
     </section>
   );
 }
