6e6a1431f48033d39ba7723d4cc15954719435dd
Timeuh <timeuuh@gmail.com>
2023-09-16 11:44:43 +0200

    feat: add error handling

--- app/_components/FormInput.tsx
+++ app/_components/FormInput.tsx
@@ -9,23 +9,27 @@ type Props = {
   value: string | number;
   valueType: string;
   changeValue: (event: ChangeEvent<HTMLInputElement>, valueType: string) => void;
+  error: string;
 };
 
-export default function FormInput({image, text, type, value, changeValue, valueType}: Props) {
+export default function FormInput({image, text, type, value, changeValue, valueType, error}: Props) {
   return (
     <div className={'flex h-fit w-full flex-col space-y-2'}>
       <div className={'flex flex-row items-center space-x-4'}>
         <Image src={image} alt={''} height={0} width={0} sizes={'100vw'} className={'h-auto w-12 xl:w-16'} />
         <h2 className={'pt-2 text-3xl xl:text-4xl'}>{text}</h2>
       </div>
-      <BaseContainer interaction={'hover'}>
+      <BaseContainer interaction={'hover'} borderColor={error !== '' ? 'error' : null}>
         <input
           type={type}
-          className={'h-10 w-[80vw] border-none bg-transparent p-2 outline-none xl:h-14 xl:w-[30vw] xl:text-xl'}
+          className={
+            'h-10 w-[80vw] border-none bg-transparent p-2 placeholder-red-500 outline-none xl:h-14 xl:w-[30vw] xl:text-xl'
+          }
           value={value}
           onChange={(event) => {
             changeValue(event, valueType);
           }}
+          placeholder={error}
         />
       </BaseContainer>
     </div>
