a5c5eff6cdd2c08275d99dada41e4e437129361d
Timeuh <timeuuh@gmail.com>
2023-09-16 11:08:15 +0200

    feat: create admin login page

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,25 @@
+import BackButton from '@components/BackButton';
+import Image from 'next/image';
+import LoginForm from '@components/admin/login/LoginForm';
+
+export default function Login() {
+  return (
+    <section className={'h-screen w-full bg-gradient-to-b from-light-purple to-light-blue'}>
+      <div className={'flex h-full w-full flex-col items-center space-y-12 pt-24'}>
+        <BackButton />
+        <div className={'flex flex-col items-center'}>
+          <Image
+            src={'/img/admin/login/admin-light.png'}
+            alt={'administrateur'}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-16'}
+          />
+          <h1 className={'text-5xl'}>Administrateur</h1>
+        </div>
+        <LoginForm />
+      </div>
+    </section>
+  );
+}
