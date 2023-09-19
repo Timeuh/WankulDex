aa8727878864204e72d53c9f75cad3f905c2b34f
Timeuh <timeuuh@gmail.com>
2023-09-19 21:42:44 +0200

    fix: divide Date.getTime per 1000 to get time in seconds to compare to exp of jwt

--- app/_utils/isTokenValid.ts
+++ app/_utils/isTokenValid.ts
@@ -3,5 +3,5 @@ import jwtDecode from 'jwt-decode';
 
 export default function isTokenValid(token: string) {
   const decodedToken: DecodedToken = jwtDecode(token);
-  return decodedToken.exp >= new Date('now').getTime();
+  return decodedToken.exp >= Math.floor(new Date().getTime() / 1000);
 }
