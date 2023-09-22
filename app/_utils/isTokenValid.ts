8f49ed2ade65b05a8cc360389a3d948b73020f53
Timeuh <timeuuh@gmail.com>
2023-09-22 10:43:53 +0200

    refactor: rename function + use universal cookie for token check

--- app/_utils/isTokenValid.ts
+++
@@ -1,7 +0,0 @@
-import {DecodedToken} from '@/app/_utils/appTypes';
-import jwtDecode from 'jwt-decode';
-
-export default function isTokenValid(token: string) {
-  const decodedToken: DecodedToken = jwtDecode(token);
-  return decodedToken.exp >= Math.floor(new Date().getTime() / 1000);
-}
