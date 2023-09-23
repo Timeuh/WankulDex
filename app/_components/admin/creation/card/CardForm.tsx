0b42b456650ebd60e44438c8c719aec8363dd779
Timeuh <timeuuh@gmail.com>
2023-09-23 21:58:26 +0200

    feat: create card form component

---
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
+
+export default function CardForm() {
+  return (
+    <form action='noredirect'>
+      <CardFirstRowForm />
+    </form>
+  );
+}
