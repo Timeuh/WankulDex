619cc5f50aac0d0ec62495adc27efb259c998356
Timeuh <timeuuh@gmail.com>
2023-09-25 20:39:14 +0200

    feat: create custom hook to fetch artists from api

---
+++ app/_hooks/useArtists.tsx
@@ -0,0 +1,34 @@
+import {useQuery} from 'react-query';
+import {API_BASE_URL, QUERY_STALE_TIME} from '@/app/_utils/appGlobals';
+import {Artist, Artists} from '@/app/_utils/appTypes';
+import artistSchema from '@/app/_zod/ArtistSchema';
+
+const getArtists = async () => {
+  return await fetch(`${API_BASE_URL}/artist`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((artists: Artists) => {
+      const allArtists: Array<Artist> = [];
+
+      artists.artists.forEach((artist: Artist) => {
+        if (artistSchema.safeParse(artist).success) {
+          allArtists.push(artist);
+        }
+      });
+
+      return allArtists;
+    });
+};
+
+export default function useArtists() {
+  return useQuery(
+    ['artists'],
+    () => {
+      return getArtists();
+    },
+    {
+      staleTime: QUERY_STALE_TIME,
+    },
+  );
+}
