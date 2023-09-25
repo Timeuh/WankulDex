e499eee5f8dc5b5b7c5faaf68c15942b086cef1d
Timeuh <timeuuh@gmail.com>
2023-09-25 21:28:22 +0200

    feat: create custom hook to fetch characters from api

---
+++ app/_hooks/useCharacters.tsx
@@ -0,0 +1,34 @@
+import {useQuery} from 'react-query';
+import {API_BASE_URL, QUERY_STALE_TIME} from '@/app/_utils/appGlobals';
+import {Character, Characters} from '@/app/_utils/appTypes';
+import characterSchema from '@/app/_zod/CharacterSchema';
+
+const getCharacters = async () => {
+  return await fetch(`${API_BASE_URL}/character`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((characters: Characters) => {
+      const allCharacters: Array<Character> = [];
+
+      characters.characters.forEach((character: Character) => {
+        if (characterSchema.safeParse(character).success) {
+          allCharacters.push(character);
+        }
+      });
+
+      return allCharacters;
+    });
+};
+
+export default function useCharacters() {
+  return useQuery(
+    ['characters'],
+    () => {
+      return getCharacters();
+    },
+    {
+      staleTime: QUERY_STALE_TIME,
+    },
+  );
+}
