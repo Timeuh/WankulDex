2cbb5268201c91903950fb18d3503ba4b9edc7b9
Timeuh <timeuuh@gmail.com>
2023-09-25 21:10:14 +0200

    feat: use value and value changer

--- app/_components/form/FormSelect.tsx
+++ app/_components/form/FormSelect.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
 import BaseContainer from '@components/BaseContainer';
 import {SelectOption} from '@/app/_utils/appTypes';
+import {ChangeEvent} from 'react';
 
 type Props = {
   image: string;
   text: string;
   options: Array<SelectOption>;
+  value: number;
+  changeValue: (event: ChangeEvent<HTMLSelectElement>) => void;
 };
 
-export default function FormSelect({image, text, options}: Props) {
+export default function FormSelect({image, text, options, value, changeValue}: Props) {
   return (
     <div className={'flex h-fit w-full flex-col space-y-2'}>
       <div className={'flex flex-row items-center space-x-4'}>
@@ -20,6 +23,10 @@ export default function FormSelect({image, text, options}: Props) {
           className={
             'h-10 w-[80vw] border-none bg-transparent p-2 text-xl placeholder-red-500 outline-none xl:h-14 xl:w-[30vw]'
           }
+          value={value}
+          onChange={(event) => {
+            changeValue(event);
+          }}
         >
           {options.map((option: SelectOption) => {
             return (
