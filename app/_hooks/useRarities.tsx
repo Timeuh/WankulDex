6c4816de88955aaa1bb00c347c7a312af4a26a14
Timeuh <timeuuh@gmail.com>
2023-09-25 21:51:58 +0200

    feat: create hook to get rarities from api

---
+++ app/_hooks/useRarities.tsx
@@ -0,0 +1,34 @@
+import {useQuery} from 'react-query';
+import {API_BASE_URL, QUERY_STALE_TIME} from '@/app/_utils/appGlobals';
+import {Rarities, Rarity} from '@/app/_utils/appTypes';
+import raritySchema from '@/app/_zod/RaritySchema';
+
+const getRarities = async () => {
+  return await fetch(`${API_BASE_URL}/rarity`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((rarities: Rarities) => {
+      const allRarities: Array<Rarity> = [];
+
+      rarities.rarities.forEach((rarity: Rarity) => {
+        if (raritySchema.safeParse(rarity).success) {
+          allRarities.push(rarity);
+        }
+      });
+
+      return allRarities;
+    });
+};
+
+export default function useRarities() {
+  return useQuery(
+    ['rarities'],
+    () => {
+      return getRarities();
+    },
+    {
+      staleTime: QUERY_STALE_TIME,
+    },
+  );
+}
