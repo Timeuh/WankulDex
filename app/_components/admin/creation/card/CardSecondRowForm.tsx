cdb1943bde9d5d9375bc18fd3be7bd2429cde4fe
Timeuh <timeuuh@gmail.com>
2023-09-25 21:10:27 +0200

    feat: change current artist when select value change

--- app/_components/admin/creation/card/CardSecondRowForm.tsx
+++ app/_components/admin/creation/card/CardSecondRowForm.tsx
@@ -2,13 +2,25 @@
 
 import FormSelect from '@components/form/FormSelect';
 import useArtistSelect from '@hooks/admin/create/cards/useArtistSelect';
+import {ChangeEvent, useState} from 'react';
 
 export default function CardSecondRowForm() {
   const {artistSelectOptions} = useArtistSelect();
+  const [currentArtist, setCurrentArtist] = useState<number>(1);
+
+  const changeArtist = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCurrentArtist(parseInt(event.target.value));
+  };
 
   return (
     <div className={'space-y-6'}>
-      <FormSelect image={'/img/admin/create/cards/pen-light.png'} text={'Artiste'} options={artistSelectOptions} />
+      <FormSelect
+        image={'/img/admin/create/cards/pen-light.png'}
+        text={'Artiste'}
+        options={artistSelectOptions}
+        value={currentArtist}
+        changeValue={changeArtist}
+      />
     </div>
   );
 }
