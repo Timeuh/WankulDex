980cffe5201c80ad2382207782903e74557c83bf
Timeuh <timeuuh@gmail.com>
2023-09-25 20:57:58 +0200

    feat: use second card form row component

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
+import CardSecondRowForm from '@components/admin/creation/card/CardSecondRowForm';
 
 export default function CardForm() {
   return (
-    <form action='noredirect'>
+    <form action='noredirect' className={'space-y-6'}>
       <CardFirstRowForm />
+      <CardSecondRowForm />
     </form>
   );
 }
