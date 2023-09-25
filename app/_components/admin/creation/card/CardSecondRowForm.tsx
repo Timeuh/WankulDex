f4abc88da8a6986f40aa01dcffc9d9dba89e699a
Timeuh <timeuuh@gmail.com>
2023-09-25 21:28:56 +0200

    feat: use character select options hook in a form select

--- app/_components/admin/creation/card/CardSecondRowForm.tsx
+++ app/_components/admin/creation/card/CardSecondRowForm.tsx
@@ -3,13 +3,21 @@
 import FormSelect from '@components/form/FormSelect';
 import useArtistSelect from '@hooks/admin/create/cards/useArtistSelect';
 import {ChangeEvent, useState} from 'react';
+import useCharacterSelect from '@hooks/admin/create/cards/useCharacterSelect';
 
 export default function CardSecondRowForm() {
   const {artistSelectOptions} = useArtistSelect();
+  const {characterSelectOptions} = useCharacterSelect();
+
   const [currentArtist, setCurrentArtist] = useState<number>(1);
+  const [currentCharacter, setCurrentCharacter] = useState<number>(1);
 
   const changeArtist = (event: ChangeEvent<HTMLSelectElement>) => {
     setCurrentArtist(parseInt(event.target.value));
+  };
+
+  const changeCharacter = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCurrentCharacter(parseInt(event.target.value));
   };
 
   return (
@@ -20,6 +28,13 @@ export default function CardSecondRowForm() {
         options={artistSelectOptions}
         value={currentArtist}
         changeValue={changeArtist}
+      />
+      <FormSelect
+        image={'/img/admin/create/cards/wankul-light.png'}
+        text={'Personnage'}
+        options={characterSelectOptions}
+        value={currentCharacter}
+        changeValue={changeCharacter}
       />
     </div>
   );
