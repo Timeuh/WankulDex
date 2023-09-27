27813fe2bf553de87f1ea60be662b08a88078195
Timeuh <timeuuh@gmail.com>
2023-09-27 10:29:16 +0200

    feat: use CardDescriptionContextProvider

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
 import CardSecondRowForm from '@components/admin/creation/card/CardSecondRowForm';
+import CardDescriptionContextProvider from '@providers/admin/creation/card/CardDescriptionContextProvider';
 
 export default function CardForm() {
   return (
     <form action='noredirect' className={'space-y-6'}>
-      <CardFirstRowForm />
-      <CardSecondRowForm />
+      <CardDescriptionContextProvider>
+        <CardFirstRowForm />
+        <CardSecondRowForm />
+      </CardDescriptionContextProvider>
     </form>
   );
 }
