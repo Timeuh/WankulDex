9a22f0f74cdde938cbd4ddff9812a5dcca00ff41
Timeuh <timeuuh@gmail.com>
2023-09-29 16:16:54 +0200

    feat: use card related context and log it on submit

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -1,46 +1,45 @@
-'use client';
-
 import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
 import CardSecondRowForm from '@components/admin/creation/card/CardSecondRowForm';
-import CardDescriptionContextProvider from '@providers/admin/creation/card/CardDescriptionContextProvider';
-import CardContextProvider from '@providers/admin/creation/card/CardContextProvider';
+import {useCardDescriptionContext} from '@providers/admin/creation/card/CardDescriptionContextProvider';
+import {useCardContext} from '@providers/admin/creation/card/CardContextProvider';
 import CardThirdRowForm from '@components/admin/creation/card/CardThirdRowForm';
 import BaseContainer from '@components/BaseContainer';
 import Image from 'next/image';
 import {MouseEvent} from 'react';
 
 export default function CardForm() {
+  const {cardContext, updateCard} = useCardContext();
+  const {cardDescription, updateDescription} = useCardDescriptionContext();
+
   const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+    console.log(cardContext);
+    console.log(cardDescription);
   };
 
   return (
     <form action='noredirect' className={'grid-cols-3 gap-6 space-y-6 pb-6 xl:grid xl:w-5/6 xl:space-y-0'}>
-      <CardDescriptionContextProvider>
-        <CardContextProvider>
-          <CardFirstRowForm />
-          <CardSecondRowForm />
-          <CardThirdRowForm />
-          <div className={'col-span-3 place-self-center py-6'}>
-            <BaseContainer interaction={'hover'}>
-              <button
-                className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
-                onClick={handleSubmit}
-              >
-                <Image
-                  src={'/img/admin/create/cards/add-light.png'}
-                  alt={'créer une carte'}
-                  width={0}
-                  height={0}
-                  sizes={'100vw'}
-                  className={'h-auto w-8 xl:w-10'}
-                />
-                <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
-              </button>
-            </BaseContainer>
-          </div>
-        </CardContextProvider>
-      </CardDescriptionContextProvider>
+      <CardFirstRowForm />
+      <CardSecondRowForm />
+      <CardThirdRowForm />
+      <div className={'col-span-3 place-self-center py-6'}>
+        <BaseContainer interaction={'hover'}>
+          <button
+            className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
+            onClick={handleSubmit}
+          >
+            <Image
+              src={'/img/admin/create/cards/add-light.png'}
+              alt={'créer une carte'}
+              width={0}
+              height={0}
+              sizes={'100vw'}
+              className={'h-auto w-8 xl:w-10'}
+            />
+            <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
+          </button>
+        </BaseContainer>
+      </div>
     </form>
   );
 }
