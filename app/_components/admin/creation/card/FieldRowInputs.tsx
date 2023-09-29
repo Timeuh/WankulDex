d20867da1e2c924d127354d03eea34d610503412
Timeuh <timeuuh@gmail.com>
2023-09-29 15:02:16 +0200

    feat: create field form text areas component

---
+++ app/_components/admin/creation/card/FieldRowInputs.tsx
@@ -0,0 +1,38 @@
+import FormTextArea from '@components/form/FormTextArea';
+import {useCardDescriptionContext} from '@providers/admin/creation/card/CardDescriptionContextProvider';
+
+export default function FieldRowInputs() {
+  const {cardDescription, updateDescription} = useCardDescriptionContext();
+
+  return (
+    <section id={'field-textareas'} className={'space-y-6'}>
+      <FormTextArea
+        text={'Effet Gagnant'}
+        value={cardDescription.winner_effect.value as string}
+        error={cardDescription.winner_effect.error}
+        image={'/img/admin/create/cards/first-light.png'}
+        changeValue={(event) => {
+          updateDescription(event.target.value, 'winner_effect');
+        }}
+      />
+      <FormTextArea
+        text={'Effet Perdant'}
+        value={cardDescription.looser_effect.value as string}
+        error={cardDescription.looser_effect.error}
+        image={'/img/admin/create/cards/last-light.png'}
+        changeValue={(event) => {
+          updateDescription(event.target.value, 'looser_effect');
+        }}
+      />
+      <FormTextArea
+        text={'Special'}
+        value={cardDescription.special.value as string}
+        error={cardDescription.special.error}
+        image={'/img/admin/create/cards/special-light.png'}
+        changeValue={(event) => {
+          updateDescription(event.target.value, 'special');
+        }}
+      />
+    </section>
+  );
+}
