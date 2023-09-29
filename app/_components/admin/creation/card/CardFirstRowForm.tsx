a3fb337108188593efeb331f45a1732feff2c715
Timeuh <timeuuh@gmail.com>
2023-09-29 09:20:35 +0200

    refactor: replace custom states by card context states

--- app/_components/admin/creation/card/CardFirstRowForm.tsx
+++ app/_components/admin/creation/card/CardFirstRowForm.tsx
@@ -1,101 +1,55 @@
 'use client';
 
-import {ChangeEvent, useState} from 'react';
-import {CardFirstRowNames, InputField} from '@/app/_utils/appTypes';
 import FormInput from '@components/form/FormInput';
+import {useCardContext} from '@providers/admin/creation/card/CardContextProvider';
 
 export default function CardFirstRowForm() {
-  const [id, setId] = useState<InputField>({
-    value: 1,
-    error: '',
-  });
-  const [name, setName] = useState<InputField>({
-    value: '',
-    error: '',
-  });
-  const [collection, setCollection] = useState<InputField>({
-    value: '',
-    error: '',
-  });
-  const [imageName, setImageName] = useState<InputField>({
-    value: '',
-    error: '',
-  });
-
-  const changeValue = (event: ChangeEvent<HTMLInputElement>, valueType: CardFirstRowNames) => {
-    const newValue = event.target.value;
-
-    switch (valueType) {
-      case 'ID':
-        if (isNaN(parseInt(newValue))) {
-          return;
-        }
-
-        const parsedValue = parseInt(event.target.value);
-        setId({...id, value: parsedValue < 1 ? 1 : parsedValue});
-        return;
-
-      case 'Name':
-        setName({...name, value: newValue});
-        return;
-
-      case 'Collection':
-        setCollection({...collection, value: newValue});
-        return;
-
-      case 'ImageName':
-        setImageName({...imageName, value: newValue});
-        return;
-
-      default:
-        return;
-    }
-  };
+  const {cardContext, updateCard} = useCardContext();
 
   return (
     <div className={'space-y-6'}>
       <FormInput
         text={'ID'}
         error={''}
-        value={id.value}
+        value={cardContext.id.value}
         type={'number'}
         image={'/img/admin/create/cards/hashtag-light.png'}
         valueType={'number'}
         changeValue={(event) => {
-          changeValue(event, 'ID');
+          updateCard(event, 'id');
         }}
       />
       <FormInput
         text={'Nom'}
         error={''}
-        value={name.value}
+        value={cardContext.name.value}
         type={'text'}
         image={'/img/admin/create/cards/name-light.png'}
         valueType={'string'}
         changeValue={(event) => {
-          changeValue(event, 'Name');
+          updateCard(event, 'name');
         }}
       />
       <FormInput
         text={'Collection'}
         error={''}
-        value={collection.value}
+        value={cardContext.collection.value}
         type={'text'}
         image={'/img/admin/create/cards/collection-light.png'}
         valueType={'string'}
         changeValue={(event) => {
-          changeValue(event, 'Collection');
+          updateCard(event, 'collection');
         }}
       />
       <FormInput
         text={'Nom de l’image'}
         error={''}
-        value={imageName.value}
+        value={cardContext.image.value}
         type={'text'}
         image={'/img/admin/create/cards/picture-light.png'}
         valueType={'string'}
         changeValue={(event) => {
-          changeValue(event, 'ImageName');
+          updateCard(event, 'image');
         }}
       />
     </div>
