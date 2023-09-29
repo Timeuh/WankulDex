078323a56958088077896e480ed019f8cefebbb5
Timeuh <timeuuh@gmail.com>
2023-09-29 15:09:27 +0200

    feat: use card third row form component

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -4,6 +4,7 @@ import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
 import CardSecondRowForm from '@components/admin/creation/card/CardSecondRowForm';
 import CardDescriptionContextProvider from '@providers/admin/creation/card/CardDescriptionContextProvider';
 import CardContextProvider from '@providers/admin/creation/card/CardContextProvider';
+import CardThirdRowForm from '@components/admin/creation/card/CardThirdRowForm';
 
 export default function CardForm() {
   return (
@@ -12,6 +13,7 @@ export default function CardForm() {
         <CardContextProvider>
           <CardFirstRowForm />
           <CardSecondRowForm />
+          <CardThirdRowForm />
         </CardContextProvider>
       </CardDescriptionContextProvider>
     </form>
