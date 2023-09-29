01537192bac67e7c2fd12442f1ba5f0b2c42845b
Timeuh <timeuuh@gmail.com>
2023-09-29 09:12:06 +0200

    feat: use CardContext for card form

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -3,13 +3,16 @@
 import CardFirstRowForm from '@components/admin/creation/card/CardFirstRowForm';
 import CardSecondRowForm from '@components/admin/creation/card/CardSecondRowForm';
 import CardDescriptionContextProvider from '@providers/admin/creation/card/CardDescriptionContextProvider';
+import CardContextProvider from '@providers/admin/creation/card/CardContextProvider';
 
 export default function CardForm() {
   return (
     <form action='noredirect' className={'space-y-6'}>
       <CardDescriptionContextProvider>
-        <CardFirstRowForm />
-        <CardSecondRowForm />
+        <CardContextProvider>
+          <CardFirstRowForm />
+          <CardSecondRowForm />
+        </CardContextProvider>
       </CardDescriptionContextProvider>
     </form>
   );
