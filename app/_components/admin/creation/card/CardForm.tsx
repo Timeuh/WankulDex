73ed3644c297e21f769be493bfb6b0a4cff634bb
Timeuh <timeuuh@gmail.com>
2023-09-30 18:26:30 +0200

    feat: create card description and log the return message + display loading when creating card description

--- app/_components/admin/creation/card/CardForm.tsx
+++ app/_components/admin/creation/card/CardForm.tsx
@@ -6,10 +6,14 @@ import CardThirdRowForm from '@components/admin/creation/card/CardThirdRowForm';
 import BaseContainer from '@components/BaseContainer';
 import Image from 'next/image';
 import {MouseEvent} from 'react';
+import useCardDescriptionCreation from '@hooks/admin/create/cards/useCardDescriptionCreation';
+import Loading from '@/app/Loading';
 
 export default function CardForm() {
   const {cardContext, updateCard} = useCardContext();
   const {cardDescription, updateDescription} = useCardDescriptionContext();
+  const {isFetching: cardDescriptionFetching, refetch: refetchCardDescription} =
+    useCardDescriptionCreation(cardDescription);
 
   const updateErrors = () => {
     updateCard(
@@ -35,33 +39,40 @@ export default function CardForm() {
     event.preventDefault();
     updateErrors();
 
-    console.log(cardContext);
-    console.log(cardDescription);
+    refetchCardDescription().then((response) => {
+      console.log(response);
+    });
   };
 
   return (
-    <form action='noredirect' className={'grid-cols-3 gap-6 space-y-6 pb-6 xl:grid xl:w-5/6 xl:space-y-0'}>
-      <CardFirstRowForm />
-      <CardSecondRowForm />
-      <CardThirdRowForm />
-      <div className={'col-span-3 place-self-center py-6'}>
-        <BaseContainer interaction={'hover'}>
-          <button
-            className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
-            onClick={handleSubmit}
-          >
-            <Image
-              src={'/img/admin/create/cards/add-light.png'}
-              alt={'créer une carte'}
-              width={0}
-              height={0}
-              sizes={'100vw'}
-              className={'h-auto w-8 xl:w-10'}
-            />
-            <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
-          </button>
-        </BaseContainer>
-      </div>
-    </form>
+    <>
+      {cardDescriptionFetching ? (
+        <Loading text={'Création en cours'} />
+      ) : (
+        <form action='noredirect' className={'grid-cols-3 gap-6 space-y-6 pb-6 xl:grid xl:w-5/6 xl:space-y-0'}>
+          <CardFirstRowForm />
+          <CardSecondRowForm />
+          <CardThirdRowForm />
+          <div className={'col-span-3 place-self-center py-6'}>
+            <BaseContainer interaction={'hover'}>
+              <button
+                className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
+                onClick={handleSubmit}
+              >
+                <Image
+                  src={'/img/admin/create/cards/add-light.png'}
+                  alt={'créer une carte'}
+                  width={0}
+                  height={0}
+                  sizes={'100vw'}
+                  className={'h-auto w-8 xl:w-10'}
+                />
+                <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
+              </button>
+            </BaseContainer>
+          </div>
+        </form>
+      )}
+    </>
   );
 }
