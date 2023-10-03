02555427bc983a553acf7852ebb3feed6100bd7a
Timeuh <timeuuh@gmail.com>
2023-10-03 13:06:05 +0200

    feat: display loading state when submitting + redirect after creation

--- app/admin/create/artist/_components/ArtistForm.tsx
+++ app/admin/create/artist/_components/ArtistForm.tsx
@@ -6,6 +6,8 @@ import {ArtistCreationResponse, ArtistForm} from '@utils/appTypes';
 import BaseContainer from '@components/BaseContainer';
 import Image from 'next/image';
 import useArtistCreation from '@hooks/useArtistCreation';
+import Loading from '@components/Loading';
+import {useRouter} from 'next/navigation';
 
 export default function ArtistForm() {
   const [artistData, setArtistData] = useState<ArtistForm>({
@@ -19,8 +21,10 @@ export default function ArtistForm() {
     },
   });
   const [isError, setError] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(false);
 
   const {refetch} = useArtistCreation(artistData);
+  const router = useRouter();
 
   const updateArtist = (value: string, type: keyof ArtistForm, error: string = '') => {
     setArtistData((prevState) => {
@@ -53,10 +57,12 @@ export default function ArtistForm() {
 
   const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+    setLoading(true);
     setError(false);
     const submitError = updateErrors();
 
     if (submitError) {
+      setLoading(false);
       return;
     }
 
@@ -65,59 +71,69 @@ export default function ArtistForm() {
 
       if (responseData.error) {
         setError(true);
+        setLoading(false);
         return;
       }
-      console.log(responseData);
+
+      router.push(`/artist/${responseData.data?.id}`);
     });
   };
 
   return (
     <>
-      <h1 className={`text-center text-2xl text-red-500 ${isError ? 'block' : 'hidden'}`}>
-        Erreur pendant la création de l&apos;artiste, veuillez réessayer
-      </h1>
-      <form action='noredirect' className={'space-y-6'}>
-        <FormInput
-          value={artistData.id.value}
-          changeValue={(event) => {
-            updateArtist(event.target.value, 'id');
-          }}
-          error={artistData.id.error}
-          valueType={'number'}
-          type={'number'}
-          image={'/img/admin/create/artists/hashtag-light.png'}
-          text={'ID'}
-        />
-        <FormInput
-          value={artistData.name.value}
-          changeValue={(event) => {
-            updateArtist(event.target.value, 'name');
-          }}
-          error={artistData.name.error}
-          valueType={'string'}
-          type={'text'}
-          image={'/img/admin/create/artists/name-light.png'}
-          text={'Nom'}
-        />
-        <div className={'absolute bottom-20'}>
-          <BaseContainer interaction={'hover'}>
-            <button
-              className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
-              onClick={handleSubmit}
-            >
-              <Image
-                src={'/img/admin/create/cards/add-light.png'}
-                alt={'créer une carte'}
-                width={0}
-                height={0}
-                sizes={'100vw'}
-                className={'h-auto w-8 xl:w-10'}
-              />
-              <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
-            </button>
-          </BaseContainer>
-        </div>
-      </form>
+      {isLoading ? (
+        <>
+          <Loading text={'Création en cours'} />
+        </>
+      ) : (
+        <>
+          <h1 className={`text-center text-2xl text-red-500 ${isError ? 'block' : 'hidden'}`}>
+            Erreur pendant la création de l&apos;artiste, veuillez réessayer
+          </h1>
+          <form action='noredirect' className={'space-y-6'}>
+            <FormInput
+              value={artistData.id.value}
+              changeValue={(event) => {
+                updateArtist(event.target.value, 'id');
+              }}
+              error={artistData.id.error}
+              valueType={'number'}
+              type={'number'}
+              image={'/img/admin/create/artists/hashtag-light.png'}
+              text={'ID'}
+            />
+            <FormInput
+              value={artistData.name.value}
+              changeValue={(event) => {
+                updateArtist(event.target.value, 'name');
+              }}
+              error={artistData.name.error}
+              valueType={'string'}
+              type={'text'}
+              image={'/img/admin/create/artists/name-light.png'}
+              text={'Nom'}
+            />
+            <div className={'absolute bottom-20'}>
+              <BaseContainer interaction={'hover'}>
+                <button
+                  className={'flex h-10 w-[80vw] flex-row items-center justify-center space-x-2 xl:h-14 xl:w-[30vw]'}
+                  onClick={handleSubmit}
+                >
+                  <Image
+                    src={'/img/admin/create/cards/add-light.png'}
+                    alt={'créer une carte'}
+                    width={0}
+                    height={0}
+                    sizes={'100vw'}
+                    className={'h-auto w-8 xl:w-10'}
+                  />
+                  <h2 className={'text-xl xl:text-2xl'}>Créer</h2>
+                </button>
+              </BaseContainer>
+            </div>
+          </form>
+        </>
+      )}
     </>
   );
 }
