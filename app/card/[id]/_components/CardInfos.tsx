3b2c643d88250cd75908ebe8be8e83ed04b96eac
Timeuh <timeuuh@gmail.com>
2023-10-05 20:44:23 +0200

    delete: remove page/[id] files for a fresh start with better data fetching

--- app/card/[id]/_components/CardInfos.tsx
+++
@@ -1,7 +0,0 @@
-import {useCardDisplayContext} from '@providers/CardDisplayProvider';
-
-export default function CardInfos() {
-  const {card, isFetching} = useCardDisplayContext();
-
-  return <div>{isFetching ? <h1>Chargement...</h1> : <h1>carte {card.card.id}</h1>}</div>;
-}
