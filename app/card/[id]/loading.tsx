6a10815167c373deeedc35f73ae1ce981c7f9f09
Timeuh <timeuuh@gmail.com>
2023-10-05 21:45:31 +0200

    feat: create loading page for when the card page is loading (auto trigger)

---
+++ app/card/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function CardLoading() {
+  return (
+    <div className={'flex h-screen flex-col items-center'}>
+      <h1 className={'pt-24 text-4xl'}>Chargement</h1>
+    </div>
+  );
+}
