bab12e996b04327e7feaf7832b984abc26a3fa36
Timeuh <timeuuh@gmail.com>
2023-10-05 21:35:55 +0200

    feat: add experimental react-query hydration for server components

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -1,22 +1,31 @@
 'use client';
 
-import {ReactNode} from 'react';
-import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactNode, useState} from 'react';
 import {QUERY_STALE_TIME} from '@utils/appGlobals';
+import {ReactQueryStreamedHydration} from '@tanstack/react-query-next-experimental';
+import {QueryClient} from '@tanstack/query-core';
+import {QueryClientProvider} from '@tanstack/react-query';
 
 type Props = {
   children: ReactNode;
 };
 
 export default function Providers({children}: Props) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        staleTime: QUERY_STALE_TIME,
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          staleTime: QUERY_STALE_TIME,
+          suspense: true,
+        },
       },
-    },
-  });
+    }),
+  );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
+  );
 }
