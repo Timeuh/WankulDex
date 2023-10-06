6e94a36f2726e511c73fc4b95fcf9763252c8045
Timeuh <timeuuh@gmail.com>
2023-10-06 14:51:43 +0200

    feat: create loading card animation

---
+++ app/card/[id]/_components/LoadingCard.tsx
@@ -0,0 +1,7 @@
+export default function LoadingCard() {
+  return (
+    <div className={'mt-24 h-[40vh] w-[60vw] rounded-lg border-2 border-dark'}>
+      <div className={' h-full  w-full animate-pulse bg-dark'}></div>
+    </div>
+  );
+}
