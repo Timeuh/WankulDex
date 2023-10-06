5df524af38840979dbb52d626461dfce737e0ec8
Timeuh <timeuuh@gmail.com>
2023-10-06 16:28:35 +0200

    feat: use row type for height

--- app/card/[id]/_components/DescriptionRow.tsx
+++ app/card/[id]/_components/DescriptionRow.tsx
@@ -1,13 +1,15 @@
 import {ReactNode} from 'react';
+import {DescriptionRowType} from '@utils/appTypes';
 
 type Props = {
   image: ReactNode;
   text: ReactNode;
+  type?: DescriptionRowType;
 };
 
-export default function DescriptionRow({image, text}: Props) {
+export default function DescriptionRow({image, text, type = 'Small'}: Props) {
   return (
-    <div className={'flex w-full flex-row'}>
+    <div className={`flex w-full flex-row ${type === 'Big' ? 'h-40' : 'h-10'}`}>
       <div className={'w-10 border-e-2 border-gray-200'}>{image}</div>
       <div className={'w-full text-center text-xl'}>{text}</div>
     </div>
