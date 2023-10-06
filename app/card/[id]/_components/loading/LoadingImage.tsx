af520e94fc1d29157c64abc0d60bc44cd56f6259
Timeuh <timeuuh@gmail.com>
2023-10-06 16:43:07 +0200

    style: remove margin (handled by DescriptionRow)

--- app/card/[id]/_components/loading/LoadingImage.tsx
+++ app/card/[id]/_components/loading/LoadingImage.tsx
@@ -1,6 +1,6 @@
 export default function LoadingImage() {
   return (
-    <div className={'m-2 h-8 w-8 overflow-hidden rounded-lg border-2 border-dark'}>
+    <div className={'h-8 w-8 overflow-hidden rounded-lg border-2 border-dark'}>
       <div className={' h-full w-full animate-pulse bg-dark'}></div>
     </div>
   );
--- app/card/[id]/_components/loading/LoadingText.tsx
+++ app/card/[id]/_components/loading/LoadingText.tsx
@@ -1,6 +1,6 @@
 export default function LoadingText() {
   return (
-    <div className={'m-2 h-8 w-full overflow-hidden rounded-full border-2 border-dark'}>
+    <div className={'h-8 w-full overflow-hidden rounded-full border-2 border-dark'}>
       <div className={' h-full w-full animate-pulse bg-dark'}></div>
     </div>
   );
