c86325fe79627f5fe0dc0cac67cfdffc1f2d24cf
Timeuh <timeuuh@gmail.com>
2023-10-06 16:28:59 +0200

    style: add bottom padding

--- app/card/[id]/_components/CardDescription.tsx
+++ app/card/[id]/_components/CardDescription.tsx
@@ -33,28 +33,30 @@ type Props = {
 
 export default function CardDescription({cardType = 'Personnage', ...props}: Props) {
   return (
-    <BaseContainer>
-      <div className={'h-full w-[80vw] rounded-lg'}>
-        <DescriptionRow image={props.idImage} text={props.idText} />
-        <DescriptionRow image={props.nameImage} text={props.nameText} />
-        <DescriptionRow image={props.collectionImage} text={props.collectionText} />
-        <DescriptionRow image={props.artistImage} text={props.artistText} />
-        {cardType === 'Terrain' ? (
-          <>
-            <DescriptionRow image={props.winnerImage} text={props.winnerText} />
-            <DescriptionRow image={props.looserImage} text={props.looserText} />
-            <DescriptionRow image={props.specialImage} text={props.specialText} />
-          </>
-        ) : (
-          <>
-            <DescriptionRow image={props.effectImage} text={props.effectText} />
-            <DescriptionRow image={props.citationImage} text={props.citationText} />
-          </>
-        )}
-        <DescriptionRow image={props.characterImage} text={props.characterText} />
-        <DescriptionRow image={props.rarityImage} text={props.rarityText} />
-        <DescriptionRow image={props.typeImage} text={props.typeText} />
-      </div>
-    </BaseContainer>
+    <div className={'pb-10'}>
+      <BaseContainer>
+        <div className={'h-full w-[80vw] rounded-lg'}>
+          <DescriptionRow image={props.idImage} text={props.idText} />
+          <DescriptionRow image={props.nameImage} text={props.nameText} />
+          <DescriptionRow image={props.collectionImage} text={props.collectionText} />
+          <DescriptionRow image={props.artistImage} text={props.artistText} />
+          {cardType === 'Terrain' ? (
+            <>
+              <DescriptionRow image={props.winnerImage} text={props.winnerText} type={'Big'} />
+              <DescriptionRow image={props.looserImage} text={props.looserText} type={'Big'} />
+              <DescriptionRow image={props.specialImage} text={props.specialText} type={'Big'} />
+            </>
+          ) : (
+            <>
+              <DescriptionRow image={props.effectImage} text={props.effectText} type={'Big'} />
+              <DescriptionRow image={props.citationImage} text={props.citationText} type={'Big'} />
+            </>
+          )}
+          <DescriptionRow image={props.characterImage} text={props.characterText} />
+          <DescriptionRow image={props.rarityImage} text={props.rarityText} />
+          <DescriptionRow image={props.typeImage} text={props.typeText} />
+        </div>
+      </BaseContainer>
+    </div>
   );
 }
