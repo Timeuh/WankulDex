389e18bb2a5ede01ee2a43092337c7ac5a346297
Timeuh <timeuuh@gmail.com>
2023-10-06 17:03:08 +0200

    style: add spacing between description rows

--- app/card/[id]/_components/DescriptionRow.tsx
+++ app/card/[id]/_components/DescriptionRow.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function DescriptionRow({image, text, type = 'Small'}: Props) {
   return (
-    <div className={`flex w-full flex-row ${type === 'Big' ? 'h-40' : 'h-10'}`}>
+    <div className={`flex w-full flex-row ${type === 'Big' ? 'h-40' : 'h-10'} mb-2`}>
       <div className={'w-14 border-e-2 border-gray-200 p-2'}>{image}</div>
       <div className={'w-full p-2 text-center text-xl'}>{text}</div>
     </div>
