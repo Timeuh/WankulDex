c0cad629fce93221229ecb62c11680390c30157c
Timeuh <timeuuh@gmail.com>
2023-10-06 14:25:37 +0200

    refactor: use card fetching function

--- app/card/[id]/_hooks/useCard.tsx
+++ app/card/[id]/_hooks/useCard.tsx
@@ -1,22 +1,8 @@
-import {Card} from '@utils/appTypes';
-import cardSchema from '@app/_zod/CardSchema';
-import {API_BASE_URL} from '@utils/appGlobals';
 import {useQuery} from '@tanstack/react-query';
-
-const fetchCardById = async (id: string) => {
-  return await fetch(`${API_BASE_URL}/card/${id}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((card: Card) => {
-      if (cardSchema.safeParse(card).success) {
-        return card;
-      }
-    });
-};
+import {fetchCardById} from '@utils/fetchCardById';
 
 export default function useCard(id: string) {
-  return useQuery<Card | undefined>({
+  return useQuery({
     queryKey: ['card', id],
     queryFn: () => {
       return fetchCardById(id);
