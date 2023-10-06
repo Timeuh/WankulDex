226504dc3353ffddb7c774d2f991cac78b319631
Timeuh <timeuuh@gmail.com>
2023-10-06 11:54:09 +0200

    refactor: update for react-query v5

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -2,7 +2,6 @@
 
 import {ReactNode, useState} from 'react';
 import {QUERY_STALE_TIME} from '@utils/appGlobals';
-import {ReactQueryStreamedHydration} from '@tanstack/react-query-next-experimental';
 import {QueryClient} from '@tanstack/query-core';
 import {QueryClientProvider} from '@tanstack/react-query';
 
@@ -17,15 +16,10 @@ export default function Providers({children}: Props) {
         queries: {
           refetchOnWindowFocus: false,
           staleTime: QUERY_STALE_TIME,
-          suspense: true,
         },
       },
     }),
   );
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
