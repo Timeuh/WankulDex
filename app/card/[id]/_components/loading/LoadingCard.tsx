fe75c37c87340e55d77f131f3eb42f97b51011e1
Timeuh <timeuuh@gmail.com>
2023-10-06 16:44:30 +0200

    style: make loading card bigger

--- app/card/[id]/_components/loading/LoadingCard.tsx
+++ app/card/[id]/_components/loading/LoadingCard.tsx
@@ -1,6 +1,6 @@
 export default function LoadingCard() {
   return (
-    <div className={'mt-24 h-[40vh] w-[60vw] rounded-lg border-2 border-dark'}>
+    <div className={'mt-24 h-[50vh] w-[80vw] rounded-lg border-2 border-dark'}>
       <div className={' h-full  w-full animate-pulse bg-dark'}></div>
     </div>
   );
