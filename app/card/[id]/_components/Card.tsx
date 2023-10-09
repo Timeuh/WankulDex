f1ace1d703f6fd73cab13b75f4db5b819bebac2b
Timeuh <timeuuh@gmail.com>
2023-10-09 13:52:17 +0200

    feat: add card description content

--- app/card/[id]/_components/Card.tsx
+++ app/card/[id]/_components/Card.tsx
@@ -3,6 +3,8 @@
 import useCard from '@hooks/useCard';
 import Image from 'next/image';
 import {API_DOMAIN} from '@utils/appGlobals';
+import CardDescription from '@components/CardDescription';
+import {CardType} from '@utils/appTypes';
 
 type Props = {
   id: string;
@@ -12,7 +14,7 @@ export default function Card({id}: Props) {
   const {data} = useCard(id);
 
   return (
-    <div className={'flex h-screen w-full flex-col items-center'}>
+    <div className={'flex h-full w-full flex-col items-center space-y-6'}>
       <Image
         src={`${API_DOMAIN}/${data!.links.image}`}
         alt={data!.card.name}
@@ -20,6 +22,141 @@ export default function Card({id}: Props) {
         height={0}
         sizes={'100vw'}
         className={'mt-24 h-auto w-[80vw] rounded-lg border-2 border-dark'}
+      />
+      <CardDescription
+        idImage={
+          <Image
+            src={'/img/card/hashtag-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        idText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.id}</h2>}
+        nameImage={
+          <Image
+            src={'/img/card/name-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        nameText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.name}</h2>}
+        collectionImage={
+          <Image
+            src={'/img/card/collection-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        collectionText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.collection}</h2>}
+        artistImage={
+          <Image
+            src={'/img/card/pen-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        artistText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.artist.name}</h2>}
+        effectImage={
+          <Image
+            src={'/img/card/description-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        effectText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.effect}</h2>}
+        citationImage={
+          <Image
+            src={'/img/card/citation-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        citationText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.citation}</h2>}
+        winnerImage={
+          <Image
+            src={'/img/card/first-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        winnerText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.winner_effect}</h2>}
+        looserImage={
+          <Image
+            src={'/img/card/last-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        looserText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.looser_effect}</h2>}
+        specialImage={
+          <Image
+            src={'/img/card/special-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        specialText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.special}</h2>}
+        characterImage={
+          <Image
+            src={'/img/card/wankul-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        characterText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.character.name}</h2>}
+        rarityImage={
+          <Image
+            src={'/img/card/rarity-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        rarityText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.rarity.name}</h2>}
+        typeImage={
+          <Image
+            src={'/img/card/cards-light.png'}
+            alt={data!.card.name}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-8'}
+          />
+        }
+        typeText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.type.name}</h2>}
+        cardType={data?.card.type.name as CardType}
       />
     </div>
   );
