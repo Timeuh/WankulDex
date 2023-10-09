3390e5823d98450801ad36a16b1a86e18e911f1e
Timeuh <timeuuh@gmail.com>
2023-10-09 14:00:15 +0200

    style: match card description style with the design

--- app/card/[id]/_components/Card.tsx
+++ app/card/[id]/_components/Card.tsx
@@ -34,7 +34,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        idText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.id}</h2>}
+        idText={<h2 className={'border-grey-300'}>{data?.card.id}</h2>}
         nameImage={
           <Image
             src={'/img/card/name-light.png'}
@@ -45,7 +45,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        nameText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.name}</h2>}
+        nameText={<h2 className={'border-grey-300'}>{data?.card.name}</h2>}
         collectionImage={
           <Image
             src={'/img/card/collection-light.png'}
@@ -56,7 +56,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        collectionText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.collection}</h2>}
+        collectionText={<h2 className={'border-grey-300'}>{data?.card.collection}</h2>}
         artistImage={
           <Image
             src={'/img/card/pen-light.png'}
@@ -67,7 +67,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        artistText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.artist.name}</h2>}
+        artistText={<h2 className={'border-grey-300'}>{data?.card.artist.name}</h2>}
         effectImage={
           <Image
             src={'/img/card/description-light.png'}
@@ -78,7 +78,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        effectText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.effect}</h2>}
+        effectText={<h2 className={'border-grey-300'}>{data?.card.description.effect}</h2>}
         citationImage={
           <Image
             src={'/img/card/citation-light.png'}
@@ -89,7 +89,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        citationText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.citation}</h2>}
+        citationText={<h2 className={'border-grey-300'}>{data?.card.description.citation}</h2>}
         winnerImage={
           <Image
             src={'/img/card/first-light.png'}
@@ -100,7 +100,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        winnerText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.winner_effect}</h2>}
+        winnerText={<h2 className={'border-grey-300'}>{data?.card.description.winner_effect}</h2>}
         looserImage={
           <Image
             src={'/img/card/last-light.png'}
@@ -111,7 +111,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        looserText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.looser_effect}</h2>}
+        looserText={<h2 className={'border-grey-300'}>{data?.card.description.looser_effect}</h2>}
         specialImage={
           <Image
             src={'/img/card/special-light.png'}
@@ -122,7 +122,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        specialText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.special}</h2>}
+        specialText={<h2 className={'border-grey-300'}>{data?.card.description.special}</h2>}
         characterImage={
           <Image
             src={'/img/card/wankul-light.png'}
@@ -133,7 +133,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        characterText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.character.name}</h2>}
+        characterText={<h2 className={'border-grey-300'}>{data?.card.description.character.name}</h2>}
         rarityImage={
           <Image
             src={'/img/card/rarity-light.png'}
@@ -144,7 +144,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        rarityText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.description.rarity.name}</h2>}
+        rarityText={<h2 className={'border-grey-300'}>{data?.card.description.rarity.name}</h2>}
         typeImage={
           <Image
             src={'/img/card/cards-light.png'}
@@ -155,7 +155,7 @@ export default function Card({id}: Props) {
             className={'h-auto w-8'}
           />
         }
-        typeText={<h2 className={'border-grey-300 border-b-2'}>{data?.card.type.name}</h2>}
+        typeText={<h2 className={'border-grey-300'}>{data?.card.type.name}</h2>}
         cardType={data?.card.type.name as CardType}
       />
     </div>
--- app/card/[id]/_components/DescriptionRow.tsx
+++ app/card/[id]/_components/DescriptionRow.tsx
@@ -1,17 +1,15 @@
 import {ReactNode} from 'react';
-import {DescriptionRowType} from '@utils/appTypes';
 
 type Props = {
   image: ReactNode;
   text: ReactNode;
-  type?: DescriptionRowType;
 };
 
-export default function DescriptionRow({image, text, type = 'Small'}: Props) {
+export default function DescriptionRow({image, text}: Props) {
   return (
-    <div className={`flex w-full flex-row ${type === 'Big' ? 'h-40' : 'h-10'} mb-2`}>
+    <div className={'flex h-fit w-full flex-row'}>
       <div className={'w-14 border-e-2 border-gray-200 p-2'}>{image}</div>
-      <div className={'w-full p-2 text-center text-xl'}>{text}</div>
+      <div className={'border-grey-300 w-full border-b-2 p-2 text-center text-xl'}>{text}</div>
     </div>
   );
 }
