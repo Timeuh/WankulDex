c83d9d76ce0812b615e20da0eff5c4a56be55afc
Timeuh <timeuuh@gmail.com>
2023-10-13 21:10:16 +0200

    feat: add button to go to the real page on modal

--- app/@modal/(.)card/[id]/page.tsx
+++ app/@modal/(.)card/[id]/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Modal from '@components/Modal';
+import {useRouter} from 'next/navigation';
 
 type Props = {
   params: {
@@ -7,9 +10,19 @@ type Props = {
 };
 
 export default function Page({params}: Props) {
+  const router = useRouter();
+
   return (
     <Modal>
       <h1 className={'w-[80vw] bg-light'}>{params.id}</h1>
+      <h1
+        className={'h-14 w-14 bg-light'}
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
+        voir
+      </h1>
     </Modal>
   );
 }
