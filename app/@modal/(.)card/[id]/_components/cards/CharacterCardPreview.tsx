ea071a72a988b759d6646a00e3cc46334b4cd485
Timeuh <timeuuh@gmail.com>
2023-10-15 14:55:06 +0200

    feat: create card preview components

---
+++ app/@modal/(.)card/[id]/_components/cards/CharacterCardPreview.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import CardInfos from '@components/cards/CardInfos';
+import {Card, CardRarity} from '@utils/appTypes';
+import {API_DOMAIN} from '@utils/appGlobals';
+
+type Props = {
+  card: Card;
+};
+
+export default function CharacterCardPreview({card}: Props) {
+  return (
+    <div id={`card-${card.card.id}-preview`} className={'flex flex-col items-center'}>
+      <Image
+        src={API_DOMAIN + card.links.image}
+        alt={card.card.name}
+        width={0}
+        height={0}
+        sizes={'100vw'}
+        className={'h-auto w-5/6 rounded-t-lg border-2 border-dark xl:w-1/3'}
+      />
+      <CardInfos
+        card={card}
+        size={'character'}
+        cardName={'character'}
+        textColor={card.card.description.rarity.name as CardRarity}
+        borderColor={card.card.description.rarity.name as CardRarity}
+        image={card.card.description.rarity.name as CardRarity}
+      />
+    </div>
+  );
+}
