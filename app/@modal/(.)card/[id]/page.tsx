6d8fe8c39c05b58f87389e759645ca841e28225a
Timeuh <timeuuh@gmail.com>
2023-10-15 14:11:05 +0200

    refactor: use card details button component

--- app/@modal/(.)card/[id]/page.tsx
+++ app/@modal/(.)card/[id]/page.tsx
@@ -1,6 +1,5 @@
-'use client';
-
 import Modal from '@components/Modal';
+import DetailsButton from '@app/@modal/(.)card/[id]/_components/DetailsButton';
 
 type Props = {
   params: {
@@ -12,14 +11,7 @@ export default function Page({params}: Props) {
   return (
     <Modal>
       <h1 className={'w-[80vw] bg-light'}>{params.id}</h1>
-      <h1
-        className={'h-14 w-14 bg-light'}
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        voir
-      </h1>
+      <DetailsButton />
     </Modal>
   );
 }
