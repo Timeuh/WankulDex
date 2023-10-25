99973af2f05f7e7fe9962a0496f33b2bad740dd4
Timeuh <timeuuh@gmail.com>
2023-10-25 22:29:50 +0200

    refactor: use new way of card hover effect

--- app/_components/cards/preview/PreviewImage.tsx
+++ app/_components/cards/preview/PreviewImage.tsx
@@ -9,54 +9,90 @@ type Props = {
 };
 
 export default function PreviewImage({src, alt, cardType}: Props) {
-  const imageContainer = useRef<HTMLImageElement>(null);
+  const imageShine = useRef<HTMLDivElement>(null);
+  const image3d = useRef<HTMLDivElement>(null);
+  const shineClasses = 'xl:shine-classes';
 
   const handleMove = (event: MouseEvent<HTMLDivElement>) => {
-    if (!imageContainer.current) {
+    if (!image3d.current) {
       return;
     }
 
-    const xAxis = (window.innerWidth / 2 - event.clientX) / 10;
-    const yAxis = (window.innerHeight / 2 - event.clientY) / 10;
+    const b = image3d.current.getBoundingClientRect();
+    const X = (event.clientX - b.left) / image3d.current.clientWidth;
+    const Y = (event.clientY - b.top) / image3d.current.clientHeight;
 
-    imageContainer.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    const rX = -(X - 0.5) * 26;
+    const rY = (Y - 0.5) * 26;
+
+    image3d.current.style.transform = `rotateY(${rX}deg) rotateX(${rY}deg)`;
+
+    if (!imageShine.current) {
+      return;
+    }
+
+    const bgX = 40 + 20 * X;
+    const bgY = 40 + 20 * Y;
+
+    imageShine.current.style.setProperty('--bg-x', `${bgX}%`);
+    imageShine.current.style.setProperty('--bg-y', `${bgY}%`);
   };
 
   const handleEnter = () => {
-    if (!imageContainer.current) {
+    if (!image3d.current) {
       return;
     }
 
-    imageContainer.current.style.transition = 'none';
+    image3d.current.style.transition = 'none';
+
+    if (!imageShine.current) {
+      return;
+    }
+
+    imageShine.current.className = shineClasses;
   };
 
   const handleLeave = () => {
-    if (!imageContainer.current) {
+    if (!image3d.current) {
       return;
     }
 
-    imageContainer.current.style.transition = 'all 0.5s ease';
-    imageContainer.current.style.transform = 'rotateY(0deg) rotateX(0deg)';
+    image3d.current.style.transition = 'all 0.5s ease';
+    image3d.current.style.transform = 'rotateY(0deg) rotateX(0deg)';
+
+    if (!imageShine.current) {
+      return;
+    }
+
+    imageShine.current.className = '';
   };
 
   return (
     <div
-      className={'flex flex-row items-center justify-center px-10 py-4'}
+      className={`box-border h-auto w-[80vw] xl:relative ${
+        cardType === 'Personnage' ? 'xl:h-[50vh] xl:w-[20vw]' : 'xl:h-[30vh] xl:w-[30vw]'
+      }`}
       onMouseMove={handleMove}
       onMouseEnter={handleEnter}
       onMouseLeave={handleLeave}
     >
-      <Image
-        src={src}
-        alt={alt}
-        width={0}
-        height={0}
-        sizes={'100vw'}
-        className={`image-container h-auto w-full rounded-lg border-2 border-dark ${
-          cardType === 'Personnage' ? 'xl:w-[25vw]' : 'xl:w-[40vw]'
-        }`}
-        ref={imageContainer}
-      />
+      <div
+        className={`image-container rounded-lg xl:absolute xl:inset-0 ${cardType === 'Personnage' ? '' : 'xl:-top-24'}`}
+      >
+        <div className='rounded-lg xl:absolute xl:inset-0' ref={image3d}>
+          <div className='rounded-lg'>
+            <Image
+              src={src}
+              alt={alt}
+              width={0}
+              height={0}
+              sizes={'100vw'}
+              className={'h-auto w-full rounded-lg border-2 border-dark xl:relative'}
+            />
+          </div>
+          {cardType === 'Personnage' ? <div ref={imageShine}></div> : null}
+        </div>
+      </div>
     </div>
   );
 }
