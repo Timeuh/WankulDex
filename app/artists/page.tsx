e2b7525f95a4934dc6af6d4fdb823ec9cb7f53e9
Timeuh <timeuuh@gmail.com>
2023-10-29 21:58:06 +0100

    feat: prefetch query for artists

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,12 +1,34 @@
 import DisplayArtists from '@components/DisplayArtists';
+import {dehydrate, QueryClient} from '@tanstack/query-core';
+import {QUERY_STALE_TIME} from '@utils/appGlobals';
+import {getArtists} from '@hooks/useArtists';
+import {HydrationBoundary} from '@tanstack/react-query';
 
-export default function GetAllArtists() {
+export default async function GetAllArtists() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        staleTime: QUERY_STALE_TIME,
+      },
+    },
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ['artists'],
+    queryFn: () => {
+      return getArtists();
+    },
+  });
+
   return (
-    <section
-      id={'artists'}
-      className={'flex h-full w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
-    >
-      <DisplayArtists />
-    </section>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section
+        id={'artists'}
+        className={'flex h-full w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
+      >
+        <DisplayArtists />
+      </section>
+    </HydrationBoundary>
   );
 }
