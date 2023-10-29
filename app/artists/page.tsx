40d727c9108792fb786fa1b24a9075286e1b2e19
Timeuh <timeuuh@gmail.com>
2023-10-29 18:32:53 +0100

    feat: create artist list page

---
+++ app/artists/page.tsx
@@ -0,0 +1,10 @@
+export default function GetAllArtists() {
+  return (
+    <section
+      id={'artists'}
+      className={'flex h-screen w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
+    >
+      <h1>Artistes</h1>
+    </section>
+  );
+}
