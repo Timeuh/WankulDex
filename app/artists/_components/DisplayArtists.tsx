c007bd538050152529ae197fa6b8952316281959
Timeuh <timeuuh@gmail.com>
2023-10-29 21:05:23 +0100

    feat: use loading artist component when loading data

--- app/artists/_components/DisplayArtists.tsx
+++ app/artists/_components/DisplayArtists.tsx
@@ -3,19 +3,21 @@
 import {Artist} from '@utils/appTypes';
 import useArtists from '@hooks/useArtists';
 import DisplayArtistCard from '@components/DisplayArtistCard';
+import LoadingArtist from '@components/LoadingArtist';
 
 export default function DisplayArtists() {
   const {data} = useArtists();
-
-  if (!data) {
-    return <h1>chargement</h1>;
-  }
+  const loadingArtists = [1, 2, 3, 4, 5, 6];
 
   return (
-    <div className={'grid h-full w-full grid-cols-2 pt-24'}>
-      {data.map((artist: Artist) => {
-        return <DisplayArtistCard key={artist.artist.id} artistId={artist.artist.id} />;
-      })}
+    <div className={'grid h-full w-full grid-cols-2 place-items-center gap-6 pt-24'}>
+      {data
+        ? data.map((artist: Artist) => {
+            return <DisplayArtistCard key={artist.artist.id} artistId={artist.artist.id} />;
+          })
+        : loadingArtists.map((id: number) => {
+            return <LoadingArtist key={id} />;
+          })}
     </div>
   );
 }
