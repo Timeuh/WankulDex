5233484ae999f031239dd566a77d4ba5ac84e474
Timeuh <timeuuh@gmail.com>
2023-10-29 22:04:45 +0100

    refactor: remove loading ui (moved to loading page)

--- app/artists/_components/DisplayArtists.tsx
+++ app/artists/_components/DisplayArtists.tsx
@@ -3,21 +3,15 @@
 import {Artist} from '@utils/appTypes';
 import useArtists from '@hooks/useArtists';
 import DisplayArtistCard from '@components/DisplayArtistCard';
-import LoadingArtist from '@components/LoadingArtist';
 
 export default function DisplayArtists() {
   const {data} = useArtists();
-  const loadingArtists = [1, 2, 3, 4, 5, 6];
 
   return (
     <div className={'grid h-full w-full grid-cols-2 place-items-center gap-6 px-4 pb-6 pt-24'}>
-      {data
-        ? data.map((artist: Artist) => {
-            return <DisplayArtistCard key={artist.artist.id} artistId={artist.artist.id} />;
-          })
-        : loadingArtists.map((id: number) => {
-            return <LoadingArtist key={id} />;
-          })}
+      {data!.map((artist: Artist) => {
+        return <DisplayArtistCard key={artist.artist.id} artistId={artist.artist.id} />;
+      })}
     </div>
   );
 }
