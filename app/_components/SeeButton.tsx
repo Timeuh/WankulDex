e5a0fdacb6821f20b16a5600d9752b61ccd997ef
Timeuh <timeuuh@gmail.com>
2023-10-29 22:13:58 +0100

    style: adapt artist display to desktop

--- app/_components/SeeButton.tsx
+++ app/_components/SeeButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function SeeButton({link}: Props) {
   return (
     <BaseContainer interaction={'hover'}>
-      <Link href={link} className={'h-8 w-[35vw] text-center text-2xl'}>
+      <Link href={link} className={'h-8 w-[35vw] text-center text-2xl xl:h-12 xl:w-[15vw] xl:pt-1 xl:text-3xl'}>
         Consulter
       </Link>
     </BaseContainer>
--- app/artists/_components/DisplayArtistCard.tsx
+++ app/artists/_components/DisplayArtistCard.tsx
@@ -23,16 +23,20 @@ export default function DisplayArtistCard({artistId}: Props) {
   });
 
   return (
-    <div className={'flex flex-col items-center space-y-2 rounded-lg border-2 border-dark bg-light p-4'}>
+    <div
+      className={
+        'flex flex-col items-center space-y-2 rounded-lg border-2 border-dark bg-light p-4 xl:w-5/6 xl:space-y-6 xl:p-6'
+      }
+    >
       <Image
         src={`${API_DOMAIN}${firstCharacterCard!.links.image}`}
         alt={data.artist.name}
         width={0}
         height={0}
         sizes={'100vw'}
-        className={'h-auto w-full rounded-lg'}
+        className={'h-auto w-full rounded-lg xl:w-3/4'}
       />
-      <h1 className={'text-center text-2xl'}>{data.artist.name}</h1>
+      <h1 className={'text-center text-2xl xl:text-3xl'}>{data.artist.name}</h1>
       <SeeButton link={`/artists/${data.artist.id}`} />
     </div>
   );
