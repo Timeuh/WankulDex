a13ea1e44ac797d60e90361056624f541f0d53dc
Timeuh <timeuuh@gmail.com>
2023-10-29 22:17:09 +0100

    style: adapt artist loading to desktop

--- app/artists/_components/LoadingArtist.tsx
+++ app/artists/_components/LoadingArtist.tsx
@@ -2,7 +2,7 @@ export default function LoadingArtist() {
   return (
     <div
       className={
-        'flex h-[30vh] w-[40vw] flex-col items-center space-y-6 rounded-lg border-2 border-dark bg-light p-2 text-center'
+        'flex h-[30vh] w-[40vw] flex-col items-center space-y-6 rounded-lg border-2 border-dark bg-light p-2 text-center xl:h-[50vh] xl:w-[15vw]'
       }
     >
       <div className={'h-3/4 w-full animate-pulse rounded-lg bg-dark'}></div>
