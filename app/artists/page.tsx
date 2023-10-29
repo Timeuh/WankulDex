7620a15d8fce1d8cada733809e0256c8aa7baa3f
Timeuh <timeuuh@gmail.com>
2023-10-29 19:46:46 +0100

    refactor: use display artists component

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,10 +1,12 @@
+import DisplayArtists from '@components/DisplayArtists';
+
 export default function GetAllArtists() {
   return (
     <section
       id={'artists'}
       className={'flex h-screen w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
     >
-      <h1>Artistes</h1>
+      <DisplayArtists />
     </section>
   );
 }
