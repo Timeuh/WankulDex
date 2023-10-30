16c2deda20b7cf419d312cbf537af96e65b877f7
Timeuh <timeuuh@gmail.com>
2023-10-30 18:29:46 +0100

    style: remove top padding

--- app/artists/loading.tsx
+++ app/artists/loading.tsx
@@ -4,7 +4,7 @@ export default function ArtistsLoading() {
   const loadingArtists = [1, 2, 3, 4, 5, 6];
 
   return (
-    <div className={'grid h-full w-full grid-cols-2 place-items-center gap-6 px-4 pb-6 pt-24 xl:grid-cols-4'}>
+    <div className={'grid h-full w-full grid-cols-2 place-items-center gap-6 px-4 pb-6 xl:grid-cols-4'}>
       {loadingArtists.map((id: number) => {
         return <LoadingListItem key={id} />;
       })}
