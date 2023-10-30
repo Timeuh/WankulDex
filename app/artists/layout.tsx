6ea73d7a2dd156d8e3dd513ef9595a3c30fcb39c
Timeuh <timeuuh@gmail.com>
2023-10-30 18:31:16 +0100

    feat: display page list type

--- app/artists/layout.tsx
+++ app/artists/layout.tsx
@@ -1,5 +1,7 @@
 import {ReactNode} from 'react';
 import BackButton from '@components/BackButton';
+import BaseContainer from '@components/BaseContainer';
+import Image from 'next/image';
 
 type Props = {
   children: ReactNode;
@@ -9,11 +11,24 @@ export default function ArtistsLayout({children}: Props) {
   return (
     <section
       id={'artists'}
-      className={'flex h-full w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
+      className={'flex h-full w-full flex-col items-center space-y-6 bg-gradient-to-b from-light-blue to-light-purple'}
     >
       <div className={'pt-24'}>
         <BackButton />
       </div>
+      <BaseContainer>
+        <div className={'flex h-14 w-[80vw] flex-row items-center justify-center space-x-4 xl:h-16 xl:w-[30vw]'}>
+          <Image
+            src={'/img/pen-light.png'}
+            alt={'artistes'}
+            width={0}
+            height={0}
+            sizes={'100vw'}
+            className={'h-auto w-10 xl:w-12'}
+          />
+          <h1 className={'text-2xl xl:text-3xl'}>Artistes</h1>
+        </div>
+      </BaseContainer>
       {children}
     </section>
   );
