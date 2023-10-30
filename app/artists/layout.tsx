3b6e68fb3e65bfa6c40cc2f77e501e81c344bf3e
Timeuh <timeuuh@gmail.com>
2023-10-30 18:20:54 +0100

    feat: add back button in layout

--- app/artists/layout.tsx
+++ app/artists/layout.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from 'react';
+import BackButton from '@components/BackButton';
 
 type Props = {
   children: ReactNode;
@@ -10,6 +11,9 @@ export default function ArtistsLayout({children}: Props) {
       id={'artists'}
       className={'flex h-full w-full flex-col items-center bg-gradient-to-b from-light-blue to-light-purple'}
     >
+      <div className={'pt-24'}>
+        <BackButton />
+      </div>
       {children}
     </section>
   );
