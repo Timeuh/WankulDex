09655973e3e8430c71b76be16d3d553965053479
Timeuh <timeuuh@gmail.com>
2023-10-30 18:13:36 +0100

    refactor: use list item component

--- app/artists/_components/DisplayArtistCard.tsx
+++ app/artists/_components/DisplayArtistCard.tsx
@@ -2,10 +2,8 @@
 
 import useArtistCards from '@app/artists/_hooks/useArtistCards';
 import LoadingListItem from '@components/LoadingListItem';
-import Image from 'next/image';
-import {API_DOMAIN} from '@utils/appGlobals';
 import {Card} from '@utils/appTypes';
-import SeeButton from '@components/SeeButton';
+import ListItem from '@components/ListItem';
 
 type Props = {
   artistId: number;
@@ -23,21 +21,11 @@ export default function DisplayArtistCard({artistId}: Props) {
   });
 
   return (
-    <div
-      className={
-        'flex flex-col items-center space-y-2 rounded-lg border-2 border-dark bg-light p-4 xl:w-5/6 xl:space-y-6 xl:p-6'
-      }
-    >
-      <Image
-        src={`${API_DOMAIN}${firstCharacterCard!.links.image}`}
-        alt={data.artist.name}
-        width={0}
-        height={0}
-        sizes={'100vw'}
-        className={'h-auto w-full rounded-lg xl:w-3/4'}
-      />
-      <h1 className={'text-center text-2xl xl:text-3xl'}>{data.artist.name}</h1>
-      <SeeButton link={`/artists/${data.artist.id}`} />
-    </div>
+    <ListItem
+      alt={data.artist.name}
+      image={firstCharacterCard!.links.image}
+      text={data.artist.name}
+      seeLink={`/artists/${data.artist.id}`}
+    />
   );
 }
