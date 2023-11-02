c78438ae4c4fad54ba2d68b2f52ca9de54037189
Timeuh <timeuuh@gmail.com>
2023-11-02 17:19:31 +0100

    refactor: move app/ into src/

--- app/card/[id]/_components/loading/LoadingImage.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingImage() {
-  return (
-    <div className={'h-8 w-8 overflow-hidden rounded-lg border-2 border-dark xl:h-10 xl:w-10'}>
-      <div className={' h-full w-full animate-pulse bg-dark'}></div>
-    </div>
-  );
-}
