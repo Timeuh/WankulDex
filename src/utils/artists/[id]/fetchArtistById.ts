f26a1e5aac3a83cdbe4cfa6084b8c30f804e1fad
Timeuh <timeuuh@gmail.com>
2023-11-03 19:21:16 +0100

    refactor: return artist cards as a json object

--- src/utils/artists/[id]/fetchArtistById.ts
+++ src/utils/artists/[id]/fetchArtistById.ts
@@ -1,15 +1,7 @@
 import {API_BASE_URL} from '@utils/appGlobals';
-import {Card} from '@utils/appTypes';
-import cardSchema from '@/zod/CardSchema';
 
 export const fetchArtistById = async (id: string) => {
-  return await fetch(`${API_BASE_URL}/artist/${id}/cards`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((card: Card) => {
-      if (cardSchema.safeParse(card).success) {
-        return card;
-      }
-    });
+  return await fetch(`${API_BASE_URL}/artist/${id}/cards`).then((response) => {
+    return response.json();
+  });
 };
