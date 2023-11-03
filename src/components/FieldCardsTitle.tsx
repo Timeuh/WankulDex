24d14a7a2a5f4249dd070a8ce6fab4dbbf86b26a
Timeuh <timeuuh@gmail.com>
2023-11-03 19:39:19 +0100

    refactor: extract field cards title into dedicated component

---
+++ src/components/FieldCardsTitle.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import BaseContainer from '@components/BaseContainer';
+
+export default function FieldCardsTitle() {
+  return (
+    <BaseContainer>
+      <div className={'flex h-12 w-[83vw] flex-row items-center justify-center space-x-4 xl:w-[30vw]'}>
+        <Image
+          src={'/img/home/type-light.png'}
+          alt={'logo terrains'}
+          width={0}
+          height={0}
+          sizes={'100vw'}
+          className={'h-auto w-10'}
+        />
+        <h1 className={'text-3xl text-dark'}>Terrains</h1>
+      </div>
+    </BaseContainer>
+  );
+}
