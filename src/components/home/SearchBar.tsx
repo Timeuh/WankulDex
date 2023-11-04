6b4029d7a4b48de3b11e680608ee47b6522ed33e
Timeuh <timeuuh@gmail.com>
2023-11-04 15:27:17 +0100

    feat: implement search feature

--- src/components/home/SearchBar.tsx
+++ src/components/home/SearchBar.tsx
@@ -1,11 +1,25 @@
+'use client';
+
 import Image from 'next/image';
 import BaseContainer from '@components/BaseContainer';
+import {useCardsContext} from '@providers/CardProvider';
+import {ChangeEvent} from 'react';
 
 export default function SearchBar() {
+  const {searchCards} = useCardsContext();
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    searchCards(event.target.value);
+  };
+
   return (
     <BaseContainer interaction={'hover'}>
       <div id={'searchbar'} className={'flex h-12 w-[65vw] flex-row items-center justify-end space-x-2 xl:w-[30vw]'}>
-        <input type={'text'} className={'h-full w-full bg-transparent ps-2 text-dark outline-none'} />
+        <input
+          type={'text'}
+          className={'h-full w-full bg-transparent ps-2 text-dark outline-none'}
+          onChange={handleSearch}
+        />
         <Image
           src={'/img/home/search-light.png'}
           alt={'filtrer'}
