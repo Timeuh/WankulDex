74d8bf0636f754ca0023e0d96fe3b945e996c4a5
Timeuh <timeuuh@gmail.com>
2023-11-04 17:02:53 +0100

    refactor: use card types from database instead of hardcoded ones

--- src/components/home/filter/FilterMenu.tsx
+++ src/components/home/filter/FilterMenu.tsx
@@ -1,23 +1,24 @@
-import {Artist, Character, GenericFilter, Rarity} from '@utils/appTypes';
+import {Artist, Character, GenericFilter, Rarity, Type} from '@utils/appTypes';
 import BaseContainer from '@components/BaseContainer';
 import useArtists from '@hooks/useArtists';
 import useCharacters from '@hooks/useCharacters';
 import useRarities from '@hooks/useRarities';
 import FilterMenuCategory from '@components/home/filter/FilterMenuCategory';
+import useTypes from '@hooks/useTypes';
 
 export default function FilterMenu() {
-  const types: Array<GenericFilter> = [
-    {name: 'Terrain', id: 1},
-    {name: 'Personnage', id: 2},
-  ];
+  const {data: types} = useTypes();
   const {data: artists} = useArtists();
   const {data: characters} = useCharacters();
   const {data: rarities} = useRarities();
 
-  if (!artists || !characters || !rarities) {
+  if (!types || !artists || !characters || !rarities) {
     return;
   }
 
+  const convertedTypes: Array<GenericFilter> = types.map((type: Type) => {
+    return type.type;
+  });
   const convertedCharacters: Array<GenericFilter> = characters.map((character: Character) => {
     return character.character;
   });
@@ -32,7 +33,7 @@ export default function FilterMenu() {
     <BaseContainer>
       <div className={'grid h-full w-[80vw] grid-cols-2'}>
         <FilterMenuCategory
-          categories={types}
+          categories={convertedTypes}
           image={'/img/home/types-filter-light.png'}
           titleColor={'text-wankil-purple'}
           title={'Types'}
