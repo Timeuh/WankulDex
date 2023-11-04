b84e2d8d231af7cbbafffb71d44087128b859020
Timeuh <timeuuh@gmail.com>
2023-11-04 18:10:22 +0100

    feat: create a provider for filter menu properties

---
+++ src/providers/home/filter/FilterMenuProvider.tsx
@@ -0,0 +1,55 @@
+import {createContext, ReactNode, useContext} from 'react';
+import useTypes from '@hooks/useTypes';
+import useArtists from '@hooks/useArtists';
+import useCharacters from '@hooks/useCharacters';
+import useRarities from '@hooks/useRarities';
+import {Artist, Character, GenericFilter, Rarity, Type} from '@utils/appTypes';
+
+interface Props {
+  children: ReactNode;
+}
+
+const FilterMenuContext = createContext<{
+  convertedTypes: GenericFilter[];
+  convertedCharacters: GenericFilter[];
+  convertedArtists: GenericFilter[];
+  convertedRarities: GenericFilter[];
+} | null>(null);
+
+export default function FilterMenuProvider({children}: Props) {
+  const {data: types} = useTypes();
+  const {data: artists} = useArtists();
+  const {data: characters} = useCharacters();
+  const {data: rarities} = useRarities();
+
+  if (!types || !artists || !characters || !rarities) {
+    return;
+  }
+
+  const convertedTypes: Array<GenericFilter> = types.map((type: Type) => {
+    return type.type;
+  });
+  const convertedCharacters: Array<GenericFilter> = characters.map((character: Character) => {
+    return character.character;
+  });
+  const convertedArtists: Array<GenericFilter> = artists.map((artist: Artist) => {
+    return artist.artist;
+  });
+  const convertedRarities: Array<GenericFilter> = rarities.map((rarity: Rarity) => {
+    return rarity.rarity;
+  });
+
+  return (
+    <FilterMenuContext.Provider value={{convertedTypes, convertedCharacters, convertedArtists, convertedRarities}}>
+      {children}
+    </FilterMenuContext.Provider>
+  );
+}
+
+export function useFilterMenuContext() {
+  const filterMenuContext = useContext(FilterMenuContext);
+  if (!filterMenuContext) {
+    throw new Error('The hook useFilterMenuContext must be used within a FilterMenuProvider');
+  }
+  return filterMenuContext;
+}
