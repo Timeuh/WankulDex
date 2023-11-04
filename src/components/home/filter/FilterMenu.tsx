10fb6c4934f17b58a7c827f2706eb73e762428a3
Timeuh <timeuuh@gmail.com>
2023-11-04 18:10:52 +0100

    refactor: use filter menu provider context for filter menu categories

--- src/components/home/filter/FilterMenu.tsx
+++ src/components/home/filter/FilterMenu.tsx
@@ -1,33 +1,10 @@
-import {Artist, Character, FilterTitle, GenericFilter, Rarity, Type} from '@utils/appTypes';
+import {FilterTitle} from '@utils/appTypes';
 import BaseContainer from '@components/BaseContainer';
-import useArtists from '@hooks/useArtists';
-import useCharacters from '@hooks/useCharacters';
-import useRarities from '@hooks/useRarities';
 import FilterMenuCategory from '@components/home/filter/FilterMenuCategory';
-import useTypes from '@hooks/useTypes';
+import {useFilterMenuContext} from '@providers/home/filter/FilterMenuProvider';
 
 export default function FilterMenu() {
-  const {data: types} = useTypes();
-  const {data: artists} = useArtists();
-  const {data: characters} = useCharacters();
-  const {data: rarities} = useRarities();
-
-  if (!types || !artists || !characters || !rarities) {
-    return;
-  }
-
-  const convertedTypes: Array<GenericFilter> = types.map((type: Type) => {
-    return type.type;
-  });
-  const convertedCharacters: Array<GenericFilter> = characters.map((character: Character) => {
-    return character.character;
-  });
-  const convertedArtists: Array<GenericFilter> = artists.map((artist: Artist) => {
-    return artist.artist;
-  });
-  const convertedRarities: Array<GenericFilter> = rarities.map((rarity: Rarity) => {
-    return rarity.rarity;
-  });
+  const {convertedTypes, convertedCharacters, convertedArtists, convertedRarities} = useFilterMenuContext();
 
   return (
     <BaseContainer>
