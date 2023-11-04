4e3730553a79c34e228c2f13c2f8d81254ba9da9
Timeuh <timeuuh@gmail.com>
2023-11-04 18:51:50 +0100

    feat: handle filter menu inputs check

--- src/components/home/filter/FilterMenuCategory.tsx
+++ src/components/home/filter/FilterMenuCategory.tsx
@@ -1,14 +1,43 @@
+'use client';
+
 import Image from 'next/image';
-import {GenericFilter} from '@utils/appTypes';
+import {FilterTitle, GenericFilter} from '@utils/appTypes';
+import {useFilterMenuContext} from '@providers/home/filter/FilterMenuProvider';
+import {ChangeEvent} from 'react';
 
 interface Props {
   categories: Array<GenericFilter>;
   image: string;
-  title: string;
+  title: FilterTitle;
   titleColor: string;
 }
 
 export default function FilterMenuCategory({categories, image, title, titleColor}: Props) {
+  const {filterMenuFilters} = useFilterMenuContext();
+
+  const handleCheck = (event: ChangeEvent<HTMLInputElement>) => {
+    switch (title) {
+      case FilterTitle.RARITIES:
+        filterMenuFilters.rarities[event.target.value] = !filterMenuFilters.rarities[event.target.value];
+        break;
+
+      case FilterTitle.ARTISTS:
+        filterMenuFilters.artists[event.target.value] = !filterMenuFilters.artists[event.target.value];
+        break;
+
+      case FilterTitle.TYPES:
+        filterMenuFilters.types[event.target.value] = !filterMenuFilters.types[event.target.value];
+        break;
+
+      case FilterTitle.CHARACTERS:
+        filterMenuFilters.characters[event.target.value] = !filterMenuFilters.characters[event.target.value];
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={'m-2 flex h-fit w-5/6 flex-col overflow-hidden border-b-2 border-[#CCCCCCFF] p-2'}>
       <div className={'flex flex-row items-center space-x-2 pb-2'}>
@@ -21,7 +50,12 @@ export default function FilterMenuCategory({categories, image, title, titleColor
             <label htmlFor={category.name} className={'ps-2 text-end text-[1em] xl:text-2xl'}>
               {category.name}
             </label>
-            <input type='checkbox' value={category.name} className={'h-6 w-6 rounded-md accent-dark xl:h-8 xl:w-8'} />
+            <input
+              type='checkbox'
+              value={category.id}
+              className={'h-6 w-6 rounded-md accent-dark xl:h-8 xl:w-8'}
+              onChange={handleCheck}
+            />
           </div>
         );
       })}
