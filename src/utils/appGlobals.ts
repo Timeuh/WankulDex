ef6ed149876215684cf035baaa65d15da0270e06
Timeuh <timeuuh@gmail.com>
2024-01-19 22:25:02 +0100

    :recycle: change api link

--- src/utils/appGlobals.ts
+++ src/utils/appGlobals.ts
@@ -1,5 +1,5 @@
-export const API_DOMAIN: string = 'https://wankul-api.vercel.app';
+export const API_DOMAIN: string = 'https://wankuldex-api.timeuh.fr';
 
-export const API_BASE_URL: string = 'https://wankul-api.vercel.app/api';
+export const API_BASE_URL: string = `${API_DOMAIN}/api`;
 
 export const QUERY_STALE_TIME = 60 * 60 * 1000;
